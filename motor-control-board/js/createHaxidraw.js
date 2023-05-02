494f780c7c4c2b998d1cc8d947342b5a80983bd6
leomcelroy <leomcelroy@gmail.com>
2023-05-02 16:14:11 -0400

    cleaned up haxidraw control board folder

---
+++ motor-control-board/js/createHaxidraw.js
@@ -0,0 +1,68 @@
+import { createWebSerialBuffer } from "./createWebSerialBuffer.js";
+import { floatsToBytes } from "./converters.js";
+
+export async function createHaxidraw(rawPort) {
+  const port = await createWebSerialBuffer(rawPort);
+
+  async function moveTo(x, y) {
+    const bytes = floatsToBytes([ x, y ]);
+    
+    const msg = new Uint8Array(10);
+
+    msg[0] = 0x00;
+    bytes.forEach((byte, i) => {
+      msg[i + 1] = byte;
+    })
+    msg[bytes.length + 1] = 0x0A;
+
+    await port.write(msg);
+  }
+
+  async function setAccel(val) {
+    const msg = new Uint8Array(6);
+    const bytes = floatsToBytes([ val ]);
+    
+    msg[0] = 0x01;
+    bytes.forEach((byte, i) => {
+      msg[i + 1] = byte;
+    })
+    msg[bytes.length + 1] = 0x0A;
+
+    await port.write(msg);
+  }
+
+  async function setMaxSpeed(val) {
+    const msg = new Uint8Array(6);
+    const bytes = floatsToBytes([ val ]);
+    
+    msg[0] = 0x02;
+    bytes.forEach((byte, i) => {
+      msg[i + 1] = byte;
+    })
+    msg[bytes.length + 1] = 0x0A;
+
+    await port.write(msg);
+  }
+
+  async function servo(microseconds) {
+    const msg = new Uint8Array(4);
+    const bytes = uint16ToBytes([ microseconds ]);
+
+    msg[0] = 0x03;
+    byteView.forEach((byte, i) => {
+      msg[i + 1] = byte;
+    })
+    msg[byteView.length + 1] = 0x0A;
+    
+    await port.write(msg);
+  }
+
+  return {
+    port,
+    moveTo,
+    setAccel,
+    setMaxSpeed,
+    servo
+  }
+}
+
