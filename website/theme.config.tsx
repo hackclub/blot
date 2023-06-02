e4f5920f34519ae8bd14e6bfcaa2be41b793212d
Ella <git@ella.cx>
2023-06-02 13:42:25 -0400

    docs

---
+++ website/theme.config.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>Haxidraw</span>,
+  project: {
+    link: "https://github.com/hackclub/haxidraw",
+  },
+  docsRepositoryBase: "https://github.com/hackclub/haxidraw",
+  footer: {
+    text: "hi",
+  },
+};
+
+export default config;
