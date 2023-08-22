beeddd3da7ff316c592ead4f06f9c554fc32df58
leomcelroy <leomcelroy@gmail.com>
2023-08-22 15:30:04 -0400

    added run text function

---
+++ astro/src/lib/runTextReturnTurtles.ts
@@ -0,0 +1,36 @@
+import { Turtle, Point } from './drawingToolkit/index.js'
+import * as drawingUtils from './drawingToolkit/utils.js'
+
+
+let _returnTurtles : Turtle[] = [];
+
+const customGlobal = {
+  // setTimeout: patchedTimeout,
+  // setInterval: patchedInterval,
+  // loop,
+  // sleep,
+
+  // console: hConsole,
+
+  // drawing functions
+
+  ...drawingUtils,
+  Turtle,
+  createTurtle: (pt: Point) => new Turtle(pt),
+  lerp(start: number, end: number, t: number) {
+    return (1 - t) * start + t * end
+  },
+  drawTurtles: (...turtlesToDraw: Turtle[]) => {
+    turtlesToDraw.forEach(t => _returnTurtles.push(t))
+  },
+  setDocDimensions(w: number, h: number) { }
+}
+
+export function runTextReturnTurtles(text: string) {
+  _returnTurtles = [];
+
+  const f = new Function(...Object.keys(customGlobal), text);
+  f(...Object.values(customGlobal));
+
+  return _returnTurtles;
+}
