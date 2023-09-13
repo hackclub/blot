ed0f868bec426603c30d978c05d9fe22cce3fe8d
Speedlet2 <144054856+Speedlet2@users.noreply.github.com>
2023-09-13 15:12:51 +0100

    Add files via upload

---
+++ art/square_circles-dexter/project (1).js
@@ -0,0 +1,334 @@
+// welcome to haxidraw!
+
+const width = 130;
+const height = 130;
+const circumference = 20;
+
+setDocDimensions(width, height);
+
+const Turtle0 = createTurtle();
+const Turtle1 = createTurtle();
+const Turtle2 = createTurtle();
+const Turtle3 = createTurtle();
+const Turtle4 = createTurtle();
+const Turtle5 = createTurtle();
+const Turtle6 = createTurtle();
+const Turtle7 = createTurtle();
+const Turtle8 = createTurtle();
+const Turtle9 = createTurtle();
+const Turtle10 = createTurtle();
+const Turtle11 = createTurtle();
+const Turtle12 = createTurtle();
+const Turtle13 = createTurtle();
+const Turtle14 = createTurtle();
+const Turtle15 = createTurtle();
+const Turtle16 = createTurtle();
+const Turtle17 = createTurtle();
+const Turtle18 = createTurtle();
+const Turtle19 = createTurtle();
+const Turtle20 = createTurtle();
+const Turtle21 = createTurtle();
+const Turtle22 = createTurtle();
+const Turtle23 = createTurtle();
+const Turtle24 = createTurtle();
+const Turtle25 = createTurtle();
+const Turtle26 = createTurtle();
+const Turtle27 = createTurtle();
+const Turtle28 = createTurtle();
+const Turtle29 = createTurtle();
+const Turtle30 = createTurtle();
+const Turtle31 = createTurtle();
+const Turtle32 = createTurtle();
+const Turtle33 = createTurtle();
+const Turtle34 = createTurtle();
+
+for (let i = 0; i < 86; i++) {
+    Turtle0.forward(circumference);
+    Turtle0.left(95);
+}
+for (let i = 0; i < 86; i++) {
+    Turtle1.forward(circumference);
+    Turtle1.left(95);
+}
+for (let i = 0; i < 86; i++) {
+    Turtle2.forward(circumference);
+    Turtle2.left(95);
+}
+for (let i = 0; i < 86; i++) {
+    Turtle3.forward(circumference);
+    Turtle3.left(95);
+}
+for (let i = 0; i < 86; i++) {
+    Turtle4.forward(circumference);
+    Turtle4.left(95);
+}
+for (let i = 0; i < 86; i++) {
+    Turtle5.forward(circumference);
+    Turtle5.left(95);
+}
+for (let i = 0; i < 86; i++) {
+    Turtle6.forward(circumference);
+    Turtle6.left(95);
+}
+for (let i = 0; i < 86; i++) {
+    Turtle7.forward(circumference);
+    Turtle7.left(95);
+}
+for (let i = 0; i < 86; i++) {
+    Turtle8.forward(circumference);
+    Turtle8.left(95);
+}
+for (let i = 0; i < 86; i++) {
+    Turtle9.forward(circumference);
+    Turtle9.left(95);
+}
+for (let i = 0; i < 86; i++) {
+    Turtle10.forward(circumference);
+    Turtle10.left(95);
+}
+for (let i = 0; i < 86; i++) {
+    Turtle11.forward(circumference);
+    Turtle11.left(95);
+}
+for (let i = 0; i < 86; i++) {
+    Turtle12.forward(circumference);
+    Turtle12.left(95);
+}
+for (let i = 0; i < 86; i++) {
+    Turtle13.forward(circumference);
+    Turtle13.left(95);
+}
+for (let i = 0; i < 86; i++) {
+    Turtle14.forward(circumference);
+    Turtle14.left(95);
+}
+for (let i = 0; i < 86; i++) {
+    Turtle15.forward(circumference);
+    Turtle15.left(95);
+}
+for (let i = 0; i < 86; i++) {
+    Turtle16.forward(circumference);
+    Turtle16.left(95);
+}
+for (let i = 0; i < 86; i++) {
+    Turtle17.forward(circumference);
+    Turtle17.left(95);
+}
+for (let i = 0; i < 86; i++) {
+    Turtle18.forward(circumference);
+    Turtle18.left(95);
+}
+for (let i = 0; i < 86; i++) {
+    Turtle19.forward(circumference);
+    Turtle19.left(95);
+}
+for (let i = 0; i < 86; i++) {
+    Turtle20.forward(circumference);
+    Turtle20.left(95);
+}
+for (let i = 0; i < 86; i++) {
+    Turtle21.forward(circumference);
+    Turtle21.left(95);
+}
+for (let i = 0; i < 86; i++) {
+    Turtle22.forward(circumference);
+    Turtle22.left(95);
+}
+for (let i = 0; i < 86; i++) {
+    Turtle23.forward(circumference);
+    Turtle23.left(95);
+}
+for (let i = 0; i < 86; i++) {
+    Turtle24.forward(circumference);
+    Turtle24.left(95);
+}
+for (let i = 0; i < 86; i++) {
+    Turtle25.forward(circumference);
+    Turtle25.left(95);
+}
+for (let i = 0; i < 86; i++) {
+    Turtle26.forward(circumference);
+    Turtle26.left(95);
+}
+for (let i = 0; i < 86; i++) {
+    Turtle27.forward(circumference);
+    Turtle27.left(95);
+}
+for (let i = 0; i < 86; i++) {
+    Turtle28.forward(circumference);
+    Turtle28.left(95);
+}
+for (let i = 0; i < 86; i++) {
+    Turtle29.forward(circumference);
+    Turtle29.left(95);
+}
+for (let i = 0; i < 86; i++) {
+    Turtle30.forward(circumference);
+    Turtle30.left(95);
+}
+for (let i = 0; i < 86; i++) {
+    Turtle31.forward(circumference);
+    Turtle31.left(95);
+}
+for (let i = 0; i < 86; i++) {
+    Turtle32.forward(circumference);
+    Turtle32.left(95);
+}
+for (let i = 0; i < 86; i++) {
+    Turtle33.forward(circumference);
+    Turtle33.left(95);
+}
+for (let i = 0; i < 86; i++) {
+    Turtle34.forward(circumference);
+    Turtle34.left(95);
+}
+
+
+
+
+
+
+Turtle0.translate(
+  [width/2, height/2], 
+  Turtle0.cc
+);
+Turtle1.translate(
+  [10+width/2, height/2], 
+  Turtle1.cc
+);
+Turtle2.translate(
+  [-10+width/2, height/2], 
+  Turtle2.cc
+);
+Turtle3.translate(
+  [5+width/2, 10+height/2], 
+  Turtle3.cc
+);
+Turtle4.translate(
+  [-5+width/2, 10+height/2], 
+  Turtle4.cc
+);
+Turtle5.translate(
+  [5+width/2, -10+height/2], 
+  Turtle5.cc
+);
+Turtle6.translate(
+  [-5+width/2, -10+height/2], 
+  Turtle6.cc
+);
+Turtle7.translate(
+  [40+width/2, 40+height/2], 
+  Turtle7.cc
+);
+Turtle8.translate(
+  [50+width/2, 40+height/2], 
+  Turtle8.cc
+);
+Turtle9.translate(
+  [30+width/2, 40+height/2], 
+  Turtle9.cc
+);
+Turtle10.translate(
+  [45+width/2, 50+height/2], 
+  Turtle10.cc
+);
+Turtle11.translate(
+  [35+width/2, 50+height/2], 
+  Turtle11.cc
+);
+Turtle12.translate(
+  [45+width/2, 30+height/2], 
+  Turtle12.cc
+);
+Turtle13.translate(
+  [35+width/2, 30+height/2], 
+  Turtle13.cc
+);
+Turtle14.translate(
+  [-40+width/2, -40+height/2], 
+  Turtle14.cc
+);
+Turtle15.translate(
+  [-30+width/2, -40+height/2], 
+  Turtle15.cc
+);
+Turtle16.translate(
+  [-50+width/2, -40+height/2], 
+  Turtle16.cc
+);
+Turtle17.translate(
+  [-35+width/2, -30+height/2], 
+  Turtle17.cc
+);
+Turtle18.translate(
+  [-45+width/2, -30+height/2], 
+  Turtle18.cc
+);
+Turtle19.translate(
+  [-45+width/2, -50+height/2], 
+  Turtle19.cc
+);
+Turtle20.translate(
+  [-35+width/2, -50+height/2], 
+  Turtle20.cc
+);
+Turtle21.translate(
+  [40+width/2, -40+height/2], 
+  Turtle21.cc
+);
+Turtle22.translate(
+  [50+width/2, -40+height/2], 
+  Turtle22.cc
+);
+Turtle23.translate(
+  [30+width/2, -40+height/2], 
+  Turtle23.cc
+);
+Turtle24.translate(
+  [45+width/2, -30+height/2], 
+  Turtle24.cc
+);
+Turtle25.translate(
+  [35+width/2, -30+height/2], 
+  Turtle25.cc
+);
+Turtle26.translate(
+  [45+width/2, -50+height/2], 
+  Turtle26.cc
+);
+Turtle27.translate(
+  [35+width/2, -50+height/2], 
+  Turtle27.cc
+);
+Turtle28.translate(
+  [-40+width/2, 40+height/2], 
+  Turtle28.cc
+);
+Turtle29.translate(
+  [-30+width/2, 40+height/2], 
+  Turtle29.cc
+);
+Turtle30.translate(
+  [-50+width/2, 40+height/2], 
+  Turtle30.cc
+);
+Turtle31.translate(
+  [-35+width/2, 50+height/2], 
+  Turtle31.cc
+);
+Turtle32.translate(
+  [-45+width/2, 50+height/2], 
+  Turtle32.cc
+);
+Turtle33.translate(
+  [-45+width/2, 30+height/2], 
+  Turtle33.cc
+);
+Turtle34.translate(
+  [-35+width/2, 30+height/2], 
+  Turtle34.cc
+);
+
+drawTurtles(
+  Turtle0, Turtle1, Turtle2, Turtle3, Turtle4, Turtle5, Turtle6, Turtle7,Turtle8,Turtle9,Turtle10,Turtle11,Turtle12,Turtle13,Turtle14,Turtle15,Turtle16,Turtle17,Turtle18,Turtle19,Turtle20,Turtle21,Turtle22,Turtle23,Turtle24,Turtle25,Turtle26,Turtle27,Turtle28,Turtle29,Turtle30,Turtle31,Turtle32,Turtle33,Turtle34
+);
