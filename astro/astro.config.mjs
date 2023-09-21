5d2c9417d04b08bd9a0d2127bd4c97e344e365a6
leomcelroy <leomcelroy@gmail.com>
2023-09-21 14:11:52 -0400

    remove mdx

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config'
 import preact from '@astrojs/preact'
-// import vercel from '@astrojs/vercel/serverless'
-import mdx from '@astrojs/mdx'
 
 export default defineConfig({
   site: 'https://blot.hackclub.com',
   integrations: [
-    preact(),
-    mdx()
+    preact()
   ]
 })
