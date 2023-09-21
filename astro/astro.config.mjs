29163e9ef941401726717a8f865ee43a3337432e
leomcelroy <leomcelroy@gmail.com>
2023-09-21 12:18:13 -0400

    debugging

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config'
 import preact from '@astrojs/preact'
-import vercel from '@astrojs/vercel/serverless'
-import prefresh from '@prefresh/vite'
+// import vercel from '@astrojs/vercel/serverless'
 import mdx from '@astrojs/mdx'
 
 export default defineConfig({
@@ -12,24 +11,5 @@ export default defineConfig({
     }),
     mdx()
   ],
-  output: 'server',
-  adapter: vercel(),
-  markdown: {
-    syntaxHighlight: 'prism'
-  },
-  vite: {
-    plugins: [prefresh()],
-    esbuild: {
-      target: 'es2020'
-    },
-    optimizeDeps: {
-      esbuildOptions: {
-        target: 'es2020'
-      }
-    },
-    build: {
-      target: 'es2020'
-    }
-  },
   integrations: [preact(), mdx()]
 })
