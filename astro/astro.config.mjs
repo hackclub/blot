d2a5ea594397204e0ce6008299d691359714b3dc
leomcelroy <leomcelroy@gmail.com>
2023-09-21 21:03:23 -0400

    WIP

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,9 +1,8 @@
-import { defineConfig } from 'astro/config'
-import preact from '@astrojs/preact'
+import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://blot.hackclub.com',
-  integrations: [
-    preact()
-  ]
-})
+  integrations: [preact()]
+});
