a8ab5e35bebb68a502932793d429740673496690
leomcelroy <leomcelroy@gmail.com>
2023-10-15 22:29:26 -0400

    adding tailwind

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blot.hackclub.com',
-  integrations: [preact()]
+  integrations: [preact(), tailwind()]
 });
