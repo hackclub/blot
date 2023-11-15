fc7fb8e8ea773e0488333e12a2d4e514ec8e3bbd
Gabe Schmit <119266089+geschmit@users.noreply.github.com>
2023-11-15 14:50:19 -0600

    Update index.js

--- art/blotfont-geschmit/index.js
+++ art/blotfont-geschmit/index.js
@@ -1,1 +1,13 @@
+/**
+* BlotFont - A demo of the BlotFont library
+* This is a demo of my BlotFont library I wrote for the Hack Club Blot, which
+* makes drawing text easy to do.
+*
+* THIS IS A BUILT SOURCE! It's advised you look at the original repository
+* where this code came from. 
+* You can find it here: https://github.com/geschmit/blotfont
+*
+* You can still play around with this, though :3
+*/
+
 // todo insert code
