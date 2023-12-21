4d6ac45decd890dc2316cd4ebf19baa16d91afc2
leomcelroy <leomcelroy@gmail.com>
2023-12-21 17:21:58 -0500

    upgrade

--- astro/src/components/Help.tsx
+++ astro/src/components/Help.tsx
@@ -1,15 +1,21 @@
 import styles from './Help.module.css'
 import { useState, useEffect } from 'preact/hooks'
-import { marked } from 'marked'
+import { Marked } from 'marked'
 import { loadCodeFromString } from '../lib/loadCodeFromString.ts'
-import { Prism } from "prism.js";
+// import Prism from "prismjs";
+import hljs from 'highlight.js';
+import {markedHighlight} from "marked-highlight";
 
-marked.setOptions({
-  highlight: function (code, language) {
-    const validLanguage = Prism.languages[language] ? language : 'markup'
-    return Prism.highlight(code, Prism.languages[validLanguage], validLanguage)
-  }
-})
+const marked = new Marked(
+  markedHighlight({
+    langPrefix: 'hljs language-',
+    highlight(code, lang, info) {
+      const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+      return hljs.highlight(code, { language }).value;
+    }
+  })
+);
+
 
 export default function Help({
   toggleClose,
