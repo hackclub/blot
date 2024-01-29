cbc084c01b0fa258723bacf69329d9280925762a
Sahiti Dasari <sahitiaws@gmail.com>
2024-01-29 18:47:41 -0500

    Update index.js

--- art/flower-sahitid/index.js
+++ art/flower-sahitid/index.js
@@ -1,24 +1,28 @@
 const t = createTurtle();
 
 function drawPetal() {
-  t.forward(10);
-  t.arc(180, 10);
+  t.forward(18);
+  t.arc(186, 10);
   t.forward(25);
-  t.arc(180, 10);
+  t.arc(180, 10);  
 }
 
 function drawFlower() {
   t.jump([50,50]); 
-  for (let i = 0; i < 7; i++) {
+  for (let i = 0; i < 8; i++) {
     drawPetal();
     t.right(360 / 7);
   }
 }
 
 function drawStem() {
-  t.jump([50,50]); 
-  t.down();
-  t.goTo([50,0]); 
+  t.jump([50, 50]); 
+  for (let i = 0; i < 3; i++) {
+    t.goTo([50 - i * 3, 45]);
+    t.up();
+    t.goTo([50 - i * 3, 0]);
+    t.down();
+  }
 }
 
 t.jump([0, 0]);
@@ -26,4 +30,3 @@ t.jump([0, 0]);
 drawFlower();
 drawStem();
 drawTurtles([t]);
-
