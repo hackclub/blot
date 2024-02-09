aff51e874f39d146ef18a2360e23e2651a21472d
revaga <58123400+revaga@users.noreply.github.com>
2024-02-08 20:18:45 -0800

    Update bloomingblossoms.js

--- art/blossom-reva/bloomingblossoms.js
+++ art/blossom-reva/bloomingblossoms.js
@@ -4,22 +4,26 @@ setDocDimensions(width, height);
 
 const t = createTurtle();
 
+//l = number of rings
 for(let l = 1; l<4; l++){
     var r = randInRange(5,width/2.5) //radius
     var rotation = randIntInRange(0,205)
+    //r is the radius of the ring, r/3 is used so that there is a little bit of gap between flowers
   for(let x = -r; x < r; x=(r/3)+x){
       //r^2 = x^2 + y^2
       var y = Math.sqrt((r*r) - (x*x))
-   
+      
       for(let i = 0; i < 6; i++){
-        //smaller inwards, bigger outwards
+        //this is to create flowers on the positive y axis and the negative y axis
         const t1 = fatromb(72*i, [x,y], rotation,r/8)
         const t2 = fatromb(72*i, [-x,-y], rotation, r/8)
+        // r/8 is used for the size of the flower so it's smaller inwards, bigger outwards
         
-        //more random sizing
+        //more random sizing, this one is similar to earlier lines but it doesn't follow the "smaller inwards bigger outwards" rule
         //const t1 = fatromb(72*i, [x,y], rotation,l*6)
         //const t2 = fatromb(72*i, [-x,-y], rotation, l*6)
-        
+
+        //this moves the flowers to the center of the canvas
         t1.translate([width/2,height/2])
         t2.translate([width/2,height/2])
         t.join(t2)
@@ -38,7 +42,8 @@ function fatromb(angle, tran = [0,0], rot = 0, size=4) {
   const t = createTurtle()
   //setup
   t.right(angle)
-  
+
+    //we choose 108 and 72 because their sum equals 180
   t.forward(size)
   t.right(72)
   t.forward(size)
@@ -48,6 +53,7 @@ function fatromb(angle, tran = [0,0], rot = 0, size=4) {
   t.forward(size)
 
   t.translate(tran)
+//this rotates the flowers by a degree to add variety
   t.rotate(rot)
   return t
 }
