cef20b5a7ec4bd68a4f5074d03f7679a0c8ad909
revaga <58123400+revaga@users.noreply.github.com>
2024-02-20 16:21:21 -0800

    Update penrose.js

--- art/penrose-reva/penrose.js
+++ art/penrose-reva/penrose.js
@@ -4,16 +4,12 @@ let ruleX = "+YF--ZF[---WF--XF]+";
 let ruleY = "-WF++XF[+++YF++ZF]-";
 let ruleZ = "--YF++++WF[+ZF++++XF]--XF";
 
-var startLength = 15
+var drawLength = 6;
 
-
-//var startLength = 21;
 var theta = 36; //36 degrees, try TWO_PI / 6.0, ...
-
 //var theta = randInRange(20,60)
 
 var production = axiom;
-var drawLength = startLength;
 
 const width = 125;
 const height = 125;
@@ -31,75 +27,37 @@ drawTurtles([t]);
 //apply substitution rules to create new iteration of production string
 function iterate(production) {
     var newProduction = "";
-  
     for(let i=0; i < production.length; ++i) {
       let step = production.charAt(i);
       //if current character is 'W', replace current character
       //by corresponding rule
       if (step === 'W') {
-        newProduction = newProduction + ruleW;
+        newProduction = newProduction.substring(0,i) + ruleW + newProduction.substring(i);
       }
       else if (step === 'X') {
-        newProduction = newProduction + ruleX;
+        newProduction = newProduction.substring(0,i) + ruleX + newProduction.substring(i);
       }
       else if (step === 'Y') {
-        newProduction = newProduction + ruleY;
+        newProduction = newProduction.substring(0,i) + ruleY + newProduction.substring(i);
       }
       else if (step === 'Z') {
-        newProduction = newProduction + ruleZ;
-      }
-      else {
-        //drop all 'F' characters, don't touch other
-        //characters (i.e. '+', '-', '[', ']'
-        if (step !== 'F') {
-          newProduction = newProduction + step;
-        }
+        newProduction = newProduction.substring(0,i) + ruleZ + newProduction.substring(i);
       }
     }
-
-    drawLength = drawLength * 0.5;
     return newProduction;
 }
 
-//convert production string to a turtle graphic
-function render(t, prod) {
-        var right = true;
 
+function render(t, prod) {
     for(let i=0; i< prod.length; ++i) {
           t.jump([width/2, height/2])
-
-
-      //production = production.substring(0, production.length-1);
-
-      var step = prod.charAt(i);
-
-      //'W', 'X', 'Y', 'Z' symbols don't actually correspond to a turtle action
-      if( step === 'F') {
+      if( prod.charAt(i) === 'F') {
         for(let j=0; j < 9; j++) {
+          t.right(theta);
           t.forward(drawLength);
           t.down(-drawLength)
-          if(right){
-            t.right(theta);
-          }
-          else{
-            t.left(theta);
-          }
         }
-      }
-      else if (step === '+') {
-        right = true;
-      }
-      else if (step === '-') {
-        right = false;
-      }
-      else if (step === '[') {
-
-      }
-      else if (step === ']') {
-        prod = prod.substring(0, prod.length-1);
-
       }
     }
   return t;
-  
 }
