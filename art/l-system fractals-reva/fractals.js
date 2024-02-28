e215d97be3b18be2cf0106b593e4a4d7ee969515
revaga <58123400+revaga@users.noreply.github.com>
2024-02-27 20:16:50 -0800

    Update fractals.js

--- art/l-system fractals-reva/fractals.js
+++ art/l-system fractals-reva/fractals.js
@@ -1,10 +1,26 @@
+/*
+@title: L-System Fractals
+@author: Reva
+@snapshot: image2.png
+*/
 
-var axiom = "L";
-let ruleL = "LLR"
-let ruleR = "R"
+//Levy C Curve
+var axiom = "A";
+let ruleA = "AAF"
+let ruleF = "F"
 
 var startLength = 16;
-var theta = 90; //36 degrees
+var theta = 90;
+
+/*
+//curve 2
+var axiom = "F";
+let ruleA = "-F+AA++A+F--F-A"
+let ruleF = "F+A++A-F--FF-A+"
+
+var startLength = 4;
+var theta = 60;
+*/
 
 var production = axiom;
 var drawLength = startLength;
@@ -17,9 +33,7 @@ setDocDimensions(width, height);
 const t = createTurtle();
 
 var prod = production
-
-let generations = randIntInRange(6,11)
-
+let generations = 11;
 for (let i = 0; i < generations; i++) {
   production = iterate(production);
 }
@@ -36,13 +50,13 @@ function iterate(production) {
   var length = production.length;
   for (let i = 0; i < length; ++i) {
     let step = production.charAt(i);
-    if (step === 'L') {
-      Newproduction = Newproduction + ruleL;
-    } else if (step === 'R') {
-      Newproduction = Newproduction + ruleR;
+    if (step === 'F') {
+      Newproduction = Newproduction + ruleF;
+    } else if (step === 'A') {
+      Newproduction = Newproduction + ruleA;
     }
   }
-  drawLength = drawLength * 0.74
+  drawLength = drawLength * 0.743
   return Newproduction;
 }
 
@@ -52,11 +66,11 @@ function render(t, prod) {
   t.jump([width/2, height/2])
   for (let i = 0; i < prod.length; ++i) {
     var step = prod.charAt(i);
-    if (step == 'L') {
+    if (step == 'A') {
       t.forward(drawLength);
       t.left(theta)
   }
-    else if (step == "R"){
+    else if (step == "F"){
       t.forward(drawLength);
       t.right(theta)
     }
