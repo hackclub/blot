6596343d0e50d021032b802cf4a64fdb558479b1
Neerav Gupta <70943638+BadPotato1007@users.noreply.github.com>
2024-03-24 15:16:34 +0530

    Create index.js

---
+++ art/cool.maze-neerav/index.js
@@ -0,0 +1,47 @@
+// Check the workshop tab to get started
+
+const width = 100;
+const height = 100;
+
+setDocDimensions(width, height);
+
+const t = createTurtle();
+
+for(var h = 0; h<=100; h++){
+  
+  for(var i = 1; i<= 100; i++){
+    var yee =  randIntInRange(0,2)
+    if(yee > 1){
+      t.up()
+    }
+    else if(yee < 1){
+      t.down()
+    }
+    t.forward(1);
+  }
+  t.jump([0, h]);
+}
+t.jump([0,0]);
+t.left(90);
+for(var h = 0; h<=100; h++){
+  
+  for(var i = 1; i<= 100; i++){
+    var yee =  randIntInRange(0,2)
+    if(yee > 1){
+      t.up()
+    }
+    else if(yee < 1){
+      t.down()
+    }
+    t.forward(1);
+  }
+  t.jump([h, 0]);
+}
+t.translate(
+  [width/2, height/2], 
+  t.cc
+);
+
+drawTurtles([
+    t
+]);
