ef93f7f0d011cc5a9a1ef276caf95cf0d138e78a
Sabyasachi <142170131+Sabya19@users.noreply.github.com>
2024-04-05 13:42:51 +0530

    Update index.js

--- art/Tesla-Sabya/index.js
+++ art/Tesla-Sabya/index.js
@@ -45,25 +45,297 @@ const createCybertruckFrame = () => {
 
 
 
-  frameTurtle.up().jump([40, 16]).down()
-  for (let i = 0; i < 4; i++) {
-    frameTurtle.forward(-117 / 6);
-    frameTurtle.left(45);
-    frameTurtle.forward(403 / 12);
-    frameTurtle.right(180);
-    frameTurtle.forward(238 / 12);
-    frameTurtle.left(45);
-  };
 
-  frameTurtle.up().jump([229, 16]).down()
-  for (let i = 0; i < 4; i++) {
-    frameTurtle.forward(-131 / 6);
-    frameTurtle.left(45);
-    frameTurtle.forward(403 / 12);
-    frameTurtle.right(180);
-    frameTurtle.forward(224 / 12);
-    frameTurtle.left(45);
-  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const rim1 = () => {
+
+  frameTurtle.up().jump([39, 16]).down()
+  const width = 98;
+  const height = 36;
+  // setDocDimensions(width, height);
+
+  const center = [width / 2, height / 2];
+  const radius = width / 4.5;
+  const lines = bt.randIntInRange(8, 16); // # vertical lines
+  const spirals = bt.randIntInRange(3, 8); // # horizontal lines
+
+  // drawing the vertical lines
+  for (let i = 0; i < lines; i++) {
+    const angle = (360 / lines) * i;
+    frameTurtle.jump(center);
+    frameTurtle.setAngle(angle);
+    frameTurtle.forward(radius);
+    frameTurtle.jump(center);
+  }
+
+  for (let s = 1; s < spirals; s++) {
+    for (let i = 0; i < lines; i++) {
+      const angle = (360 / lines) * i;
+      const nextAngle = (360 / lines) * ((i + 6) % lines);
+      //random spacing between horizontal lines
+      const randomSpacing = s * (radius / spirals) + bt.randInRange(-5, 5);
+      const sp = pointCircle(center, randomSpacing, angle);
+      const ep = pointCircle(center, randomSpacing, nextAngle);
+      frameTurtle.jump(sp);
+      //drawing lines connecting the points
+      frameTurtle.setAngle(angleTowards(sp, ep));
+      frameTurtle.forward(distance(sp, ep));
+    }
+  }
+
+
+  //coords of a point on the circumference of a circle
+  function pointCircle(center, radius, angleDegrees) {
+    const angleRadians = (Math.PI / 180) * angleDegrees;
+    return [
+      center[0] + radius * Math.cos(angleRadians),
+      center[1] + radius * Math.sin(angleRadians)
+    ];
+  }
+
+  //angle formula
+  function angleTowards(from, to) {
+    const x = to[0] - from[0];
+    const y = to[1] - from[1];
+    return Math.atan2(y, x) * (180 / Math.PI);
+  }
+
+  //distance formula
+  function distance(from, to) {
+    const dx = to[0] - from[0];
+    const dy = to[1] - from[1];
+    return Math.sqrt(dx * dx + dy * dy);
+  }
+
+
+  frameTurtle.up().jump([241, 16]).down()
+  // const width = 125;
+  // const height = 125;
+  // setDocDimensions(width, height);
+
+  // const center = [width / 2, height / 2];
+  // const radius = width / 2;
+  // const lines = bt.randIntInRange(8, 16); // # vertical lines
+  // const spirals = bt.randIntInRange(3, 8); // # horizontal lines
+
+  // drawing the vertical lines
+  for (let i = 0; i < lines; i++) {
+    const angle = (360 / lines) * i;
+    frameTurtle.jump(center);
+    frameTurtle.setAngle(angle);
+    frameTurtle.forward(radius);
+    frameTurtle.jump(center);
+  }
+
+  for (let s = 1; s < spirals; s++) {
+    for (let i = 0; i < lines; i++) {
+      const angle = (360 / lines) * i;
+      const nextAngle = (360 / lines) * ((i + 6) % lines);
+      //random spacing between horizontal lines
+      const randomSpacing = s * (radius / spirals) + bt.randInRange(-5, 5);
+      const sp = pointCircle(center, randomSpacing, angle);
+      const ep = pointCircle(center, randomSpacing, nextAngle);
+      frameTurtle.jump(sp);
+      //drawing lines connecting the points
+      frameTurtle.setAngle(angleTowards(sp, ep));
+      frameTurtle.forward(distance(sp, ep));
+    }
+  }
+
+
+  //coords of a point on the circumference of a circle
+  function pointCircle(center, radius, angleDegrees) {
+    const angleRadians = (Math.PI / 180) * angleDegrees;
+    return [
+      center[0] + radius * Math.cos(angleRadians),
+      center[1] + radius * Math.sin(angleRadians)
+    ];
+  }
+
+  //angle formula
+  function angleTowards(from, to) {
+    const x = to[0] - from[0];
+    const y = to[1] - from[1];
+    return Math.atan2(y, x) * (180 / Math.PI);
+  }
+
+  //distance formula
+  function distance(from, to) {
+    const dx = to[0] - from[0];
+    const dy = to[1] - from[1];
+    return Math.sqrt(dx * dx + dy * dy);
+  }
+
+}
+
+
+
+const rim2 = () => {
+
+  frameTurtle.up().jump([66, 16]).down()
+  const width = 480;
+  const height = 36;
+  // setDocDimensions(width, height);
+
+  const center = [width / 2, height / 2];
+  const radius = width / 19.8;
+  const lines = bt.randIntInRange(8, 16); // # vertical lines
+  const spirals = bt.randIntInRange(3, 8); // # horizontal lines
+
+  // drawing the vertical lines
+  for (let i = 0; i < lines; i++) {
+    const angle = (360 / lines) * i;
+    frameTurtle.jump(center);
+    frameTurtle.setAngle(angle);
+    frameTurtle.forward(radius);
+    frameTurtle.jump(center);
+  }
+
+  for (let s = 1; s < spirals; s++) {
+    for (let i = 0; i < lines; i++) {
+      const angle = (360 / lines) * i;
+      const nextAngle = (360 / lines) * ((i + 6) % lines);
+      //random spacing between horizontal lines
+      const randomSpacing = s * (radius / spirals) + bt.randInRange(-5, 5);
+      const sp = pointCircle(center, randomSpacing, angle);
+      const ep = pointCircle(center, randomSpacing, nextAngle);
+      frameTurtle.jump(sp);
+      //drawing lines connecting the points
+      frameTurtle.setAngle(angleTowards(sp, ep));
+      frameTurtle.forward(distance(sp, ep));
+    }
+  }
+
+
+  //coords of a point on the circumference of a circle
+  function pointCircle(center, radius, angleDegrees) {
+    const angleRadians = (Math.PI / 180) * angleDegrees;
+    return [
+      center[0] + radius * Math.cos(angleRadians),
+      center[1] + radius * Math.sin(angleRadians)
+    ];
+  }
+
+  //angle formula
+  function angleTowards(from, to) {
+    const x = to[0] - from[0];
+    const y = to[1] - from[1];
+    return Math.atan2(y, x) * (180 / Math.PI);
+  }
+
+  //distance formula
+  function distance(from, to) {
+    const dx = to[0] - from[0];
+    const dy = to[1] - from[1];
+    return Math.sqrt(dx * dx + dy * dy);
+  }
+
+
+  frameTurtle.up().jump([241, 16]).down()
+  // const width = 125;
+  // const height = 125;
+  // setDocDimensions(width, height);
+
+  // const center = [width / 2, height / 2];
+  // const radius = width / 2;
+  // const lines = bt.randIntInRange(8, 16); // # vertical lines
+  // const spirals = bt.randIntInRange(3, 8); // # horizontal lines
+
+  // drawing the vertical lines
+  for (let i = 0; i < lines; i++) {
+    const angle = (360 / lines) * i;
+    frameTurtle.jump(center);
+    frameTurtle.setAngle(angle);
+    frameTurtle.forward(radius);
+    frameTurtle.jump(center);
+  }
+
+  for (let s = 1; s < spirals; s++) {
+    for (let i = 0; i < lines; i++) {
+      const angle = (360 / lines) * i;
+      const nextAngle = (360 / lines) * ((i + 6) % lines);
+      //random spacing between horizontal lines
+      const randomSpacing = s * (radius / spirals) + bt.randInRange(-5, 5);
+      const sp = pointCircle(center, randomSpacing, angle);
+      const ep = pointCircle(center, randomSpacing, nextAngle);
+      frameTurtle.jump(sp);
+      //drawing lines connecting the points
+      frameTurtle.setAngle(angleTowards(sp, ep));
+      frameTurtle.forward(distance(sp, ep));
+    }
+  }
+
+
+  //coords of a point on the circumference of a circle
+  function pointCircle(center, radius, angleDegrees) {
+    const angleRadians = (Math.PI / 180) * angleDegrees;
+    return [
+      center[0] + radius * Math.cos(angleRadians),
+      center[1] + radius * Math.sin(angleRadians)
+    ];
+  }
+
+  //angle formula
+  function angleTowards(from, to) {
+    const x = to[0] - from[0];
+    const y = to[1] - from[1];
+    return Math.atan2(y, x) * (180 / Math.PI);
+  }
+
+  //distance formula
+  function distance(from, to) {
+    const dx = to[0] - from[0];
+    const dy = to[1] - from[1];
+    return Math.sqrt(dx * dx + dy * dy);
+  }
+
+}
+rim1();
+rim2();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   drawLines(frameTurtle.lines());
 };
