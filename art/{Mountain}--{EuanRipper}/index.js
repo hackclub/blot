3eda30a7d1a08f2297a8180d39ad8c0698cb21ad
EuanRipper <154609283+EuanRipper@users.noreply.github.com>
2024-04-06 11:21:51 +0100

    rename index.js.js to index.js and updated code
    The new code is for randomly generated trees and the drawTree function.

--- art/{Mountain}--{EuanRipper}/index.js
+++ art/{Mountain}--{EuanRipper}/index.js
@@ -1,7 +1,7 @@
 /*
 @title: Mountain
 @author: Euan Ripper
-@snapshot: snapshot1.png
+@snapshot: snapshot0.png
 */
 const width = 125;
 const height = 125;
@@ -22,6 +22,25 @@ for (let i = 0; i < 21; i++) {
   t.forward(sunSize / 2);
   t.right(4.4);
 }
+//function for trees
+function drawTree(x,y,scale){
+  //const t = new bt.Turtle();
+  t.up();
+  t.goTo([x,y])
+  t.down()
+  for (let i = 0; i < 5; i++){
+    t.goTo([x,y+scale])
+    t.goTo([x+scale/6,y+scale*0.5])
+    t.goTo([x,y+scale])
+    t.goTo([x-scale/6,y+scale*0.5])
+    t.goTo([x,y+scale])
+    scale=scale-1
+  }
+
+  t.goTo([x,y])
+  return (t)
+}
+
 //draw sun
 t.up();
 t.goTo([sunSize, height])
@@ -42,10 +61,24 @@ for (let i = 0; i < mountains; i++) {
   x = width / mountains * i
   for (let i = 0; i < width / (mountains * 2); i++) {
     x = x + 1
+    var treeSeed=Math.floor(rr(5,20));
+    if(treeSeed==9){
+      t.up();
+      let tree = drawTree(x,y,treeSeed);
+      t.down();
+      t.goTo([x,y])
+    }
     y = y + rr(-1, 3)
     t.goTo([x, y])
   }
   for (let i = 0; i < width / (mountains * 2.5); i++) {
+    var treeSeed=Math.floor(rr(5,20));
+    if(treeSeed==9){
+      t.up();
+      let tree = drawTree(x,y,treeSeed);
+      t.down();
+      t.goTo([x,y])
+    }    
     x = x + 1
     y = y + rr(-3, 1)
     t.goTo([x, y])
