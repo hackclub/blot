60c72ee2713a34ddf3f5d3b299c077a825b33584
JacksonFlanders <117542146+JacksonFlanders@users.noreply.github.com>
2024-04-25 18:21:30 -0400

    Add files via upload

--- art/GenerativeClock-JackF/index.js
+++ art/GenerativeClock-JackF/index.js
@@ -1,7 +1,7 @@
 /* 
 Author: Jack F
 Title: Clock
-Screenshots: clock1.png
+Screenshots: clock1b.png
 */
 
 //It's literally just a clock, simple but beautiful. Correct 2 times a day
@@ -11,11 +11,21 @@ const height = 125;
 
 setDocDimensions(width, height);
 
-const angle = bt.randIntInRange(1,360);
+const rand = bt.randIntInRange(1,60);
+
+//the hour of the day
+const hour = rand
+
+//the minute of the day
+const minute =rand
+
 const t = new bt.Turtle();
+const m = minute*6;
+const hr = (hour*30)+(m/12)
 
 t.jump([62.5, 62.5])
-t.right(angle)
+t.setAngle(270)
+t.right(m)
 t.right(90)
 t.forward(0.25)
 t.right(90)
@@ -27,7 +37,8 @@ t.forward(0.5)
 t.right(45)
 t.forward(50)
 t.jump([62.5, 62.5])
-t.right(angle)
+t.setAngle(90)
+t.right(hr)
 t.left(90)
 t.forward(0.5)
 t.right(90)
@@ -47,14 +58,11 @@ t.right(90)
 t.forward(4)
 t.right(90)
 t.forward(4)
+t.jump([120, 62.5])
+t.setAngle(90)
+t.arc(360, 57.5)
 t.jump([5, 120])
-t.forward(115)
-t.right(90)
-t.forward(115)
-t.right(90)
-t.forward(115)
-t.right(90)
-t.forward(115)
+t.setAngle(90)
 t.jump([62.5, 122])
 t.right(180)
 t.forward(6)
@@ -67,17 +75,6 @@ t.forward(6)
 t.jump([3, 62.5])
 t.right(180)
 t.forward(6)
-t.jump([120, 120])
-t.right(135)
-t.forward(13)
-t.jump([120, 5])
-t.right(90)
-t.forward(13)
-t.jump([5, 5])
-t.right(90)
-t.forward(13)
-t.jump([5, 120])
-t.right(90)
-t.forward(13)
+
 
 drawLines(t.lines())
