deb09caded8888dd2e89a21c73f46a50fda86523
Guy Sandler <74838853+GuySandler@users.noreply.github.com>
2024-04-26 18:25:33 -0700

    Update index.js

--- art/KochSnowflake-GuySandler/index.js
+++ art/KochSnowflake-GuySandler/index.js
@@ -1,42 +1,105 @@
 /*
-@title: Koch Snowflake Fractal
+@title: Fractal Collection
 @author: Guy Sandler
-@snapshot: Level3Detail
+@snapshot: snapshot_1
 */
 
-// Koch Snowflake Fractal by Guy Sandler
-// to learn more go to khanacademy.org/math/geometry-home/geometry-volume-surface-area/koch-snowflake/v/koch-snowflake-fractal
-const detail = 5 // how much detail/loops in the function (min 0, don't go past 7 (it's possible but will break your computer))
+const FractalChoise = 0 // 0 Koch Snowflake | 1 Sierpinski Pentagon | 2 Sierpinski Triangle | 3 Tree
+const detail = 7 // how much detail/loops in the function
+const size = 85 // size of fractal
 
 const width = 125;
 const height = 125;
 setDocDimensions(width, height);
-
 const fractal = [];
-
 const t = new bt.Turtle();
 
 function snowflake(turtle, sample, length) { // recursive function because fractals repeat themself
   if (sample == 0) {
     turtle.forward(length) // ends recursive function
-  }
-  else {
+  } else {
     for (let angle of [60, -120, 60, 0]) {
-      snowflake(turtle, sample-1, length/3)
+      snowflake(turtle, sample - 1, length / 3)
       turtle.left(angle)
     }
   }
 }
-for (let i = 0; i < 3; i++) {
-  snowflake(t, detail, 100) // original run, (turtle, sample, size)
-  t.right(120)
+// pentagon
+function draw_pentagon(turtle, length) {
+  for (let i = 0; i < 5; i++) {
+    t.forward(length)
+    t.left(72)
+  }
+}
+
+function draw_sierpinski_pentagon(turtle, sample, length) {
+  if (sample == 0) {
+    draw_pentagon(t, length)
+  } else {
+    for (let i = 0; i < 5; i++) {
+      draw_sierpinski_pentagon(turtle, sample - 1, length / 2.6)
+      t.up()
+      turtle.forward(length)
+      turtle.left(72)
+      t.down()
+    }
+  }
+}
+// triangle
+function draw_triangle(turtle, length) {
+  for (let i = 0; i < 3; i++) {
+    t.forward(length)
+    t.left(120)
+  }
+}
+
+function draw_sierpinski_triangle(turtle, sample, length) {
+  if (sample == 0) {
+    draw_triangle(t, length)
+  } else {
+    for (let i = 0; i < 3; i++) {
+      draw_sierpinski_triangle(turtle, sample - 1, length / 2)
+      t.up()
+      turtle.forward(length)
+      turtle.left(120)
+      t.down()
+    }
+  }
+}
+// tree
+function tree(turtle, sample, length) {
+  if (sample > 0) {
+    turtle.forward(length)
+    turtle.right(20)
+    tree(turtle, sample - 1, length / 1.5)
+    turtle.left(40)
+    tree(turtle, sample - 1, length / 1.5)
+    turtle.right(20)
+    turtle.forward(-length)
+  }
+}
+
+if (FractalChoise == 0) {
+  for (let i = 0; i < 3; i++) {
+    snowflake(t, detail, size)
+    t.right(120)
+  }
+}
+if (FractalChoise == 1) {
+  draw_sierpinski_pentagon(t, detail, size)
+}
+if (FractalChoise == 2) {
+  draw_sierpinski_triangle(t, detail, size)
+}
+if (FractalChoise == 3) {
+  // t.right(180)
+  t.left(90)
+  tree(t, detail, size)
 }
 // add turtle to final lines
 bt.join(fractal, t.lines());
-
 // center piece
 const cc = bt.bounds(fractal).cc;
 bt.translate(fractal, [width / 2, height / 2], cc);
-
 // draw it
 drawLines(fractal, { fill: "none", stroke: "black", width: 1 });
