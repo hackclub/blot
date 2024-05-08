69ca77ba28241e51472b22f1e9b447602285fc70
Derrek Chow <derrekchow@users.noreply.github.com>
2024-05-08 14:19:48 +0800

    fix NodeSerialBuffer

--- src/haxidraw/createNodeSerialBuffer.js
+++ src/haxidraw/createNodeSerialBuffer.js
@@ -1,7 +1,11 @@
 export async function createNodeSerialBuffer(port) {
   let buffer = []
   port.open()
-  port.on('data', buffer.push)
+  port.on('data', (data) => {
+    for (let i=0; i<data.length; i++) {
+      buffer.push(data[i])
+    }
+  })
 
   return {
     write: msg => port.write(msg),
