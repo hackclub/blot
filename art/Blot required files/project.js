478d8a2ce5128cc032eda6b3360e7485bb6000a0
geethasreeg <geethashree2011@gmail.com>
2024-05-16 08:44:58 +0530

    Add files via upload

---
+++ art/Blot required files/project.js
@@ -0,0 +1,106 @@
+// welcome to blot!
+
+/*
+@title: Spirilla
+@author: Geetha Sree G
+@snapshot: Spiralled
+*/
+
+const width = 125;
+const height = 125;
+
+setDocDimensions(width, height);
+
+const finalLines = [];
+
+const t = new bt.Turtle();
+t.arc(90, 45)
+t.arc(70, 35)
+t.arc(50, 25)
+
+t.arc(90, 45)
+t.arc(70, 35)
+t.arc(50, 25)
+
+t.arc(90, 45)
+t.arc(70, 35)
+t.arc(50, 25)
+
+
+
+t.arc(90, 45)
+t.arc(70, 35)
+t.arc(50, 25)
+
+t.arc(90, 45)
+t.arc(70, 35)
+t.arc(50, 25)
+
+t.arc(90, 45)
+t.arc(70, 35)
+t.arc(50, 25)
+
+t.arc(90, 45)
+t.arc(70, 35)
+t.arc(50, 25)
+
+t.arc(90, 45)
+t.arc(70, 35)
+t.arc(50, 25)
+
+t.arc(90, 45)
+t.arc(70, 35)
+t.arc(50, 25)
+
+t.arc(90, 45)
+t.arc(70, 35)
+t.arc(50, 25)
+
+t.arc(90, 45)
+t.arc(70, 35)
+t.arc(50, 25)
+
+t.arc(90, 45)
+t.arc(70, 35)
+t.arc(50, 25)
+
+t.arc(90, 45)
+t.arc(70, 35)
+t.arc(50, 25)
+
+t.arc(90, 45)
+t.arc(70, 35)
+t.arc(50, 25)
+
+t.arc(90, 45)
+t.arc(70, 35)
+t.arc(50, 25)
+
+t.arc(90, 45)
+t.arc(70, 35)
+t.arc(50, 25)
+
+t.arc(90, 45)
+t.arc(70, 35)
+t.arc(50, 25)
+
+t.arc(90, 45)
+t.arc(70, 35)
+t.arc(50, 25)
+
+t.arc(90, 45)
+t.arc(70, 35)
+t.arc(50, 25)
+
+t.arc(90, 45)
+t.arc(70, 35)
+t.arc(50, 25)
+
+t.arc(90, 45)
+t.arc(70, 35)
+t.arc(50, 25)
+
+bt.join(finalLines, t.lines())
+bt.translate(finalLines, [width / 2, height / 2], bt.bounds(finalLines).cc)
+
+drawLines(finalLines)
