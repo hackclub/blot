3582cc256668601d81288df4ec5d94a6872b8439
bhattprayas <156197965+bhattprayas@users.noreply.github.com>
2024-06-18 09:09:36 +0545

    Add files via upload

---
+++ art/boatart-bhattprayas/index.js
@@ -0,0 +1,75 @@
+// welcome to blot!
+
+// check out this guide to learn how to program in blot
+// https://blot.hackclub.com/editor?guide=start
+
+const width = 125;
+const height = 190;
+
+setDocDimensions(width, height);
+
+// store final lines here
+const final = [];
+
+// create a boatBase
+const boatBase = [
+  [30, 150],
+  [95, 150],
+  [80, 170],
+  [45, 170],
+  [30, 150]
+];
+
+// add the boatBase to the final lines
+final.push(boatBase);
+
+// Create a mast
+const mast = [
+  [62, 150],
+  [62, 70]
+];
+
+// add the mast to the final lines
+final.push(mast);
+
+// Create a sail / triangle
+const sail = [
+  [62, 70],
+  [100, 140],
+  [62, 140],
+  [62, 70]
+];
+
+// add sail to the final lines
+final.push(sail);
+
+// Create waves under the boat
+const wave1 = [
+  [30, 170],
+  [40, 175],
+  [50, 170],
+  [60, 175],
+  [70, 170],
+  [80, 175],
+  [90, 170]
+];
+
+const wave2 = [
+  [35, 180],
+  [45, 185],
+  [55, 180],
+  [65, 185],
+  [75, 180],
+  [85, 185],
+  [95, 180]
+];
+
+// Add the waves to the final lines
+final.push(wave1);
+final.push(wave2);
+
+// transform lines using the toolkit
+bt.rotate(final, 180);
+
+// draw it
+drawLines(final);
