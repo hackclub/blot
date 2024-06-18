b97d9436989bc75881bf19bc3e2c4468d8cc71b1
aryan9190 <115806023+aryan9190@users.noreply.github.com>
2024-06-18 19:20:13 +0530

    Delete art/Rubik's Cube directory

--- art/Rubik's Cube/index.js
+++
@@ -1,2 +0,0 @@
-This attachment was blocked by gmail.
-See https://support.google.com/mail/answer/6590 for more information.
--- art/Rubik's Cube/readme.md
+++
@@ -1,1 +0,0 @@
-A blot art that generates a beautiful Rubik's Cube with random color faces each time.
