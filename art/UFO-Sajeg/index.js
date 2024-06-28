3320be64b9dbe16607a72cc94dcab1ddcff2081d
Sajeg <84874195+Sajeg@users.noreply.github.com>
2024-06-28 18:00:59 +0200

    Updated UFO from Sajeg

--- art/UFO-Sajeg/index.js
+++ art/UFO-Sajeg/index.js
@@ -1,13 +1,7 @@
-/*
- @ title: UFO
- @author: Sajeg
- @snapshot: image0.png
-*/
-
 //A flying Unknown Flying Object, or UFO in short.
 
 //Changeable parameters
-const r = bt.randIntInRange(15, 65) //Size of the top, should be <65
+const r = bt.randIntInRange(15, 45) //Size of the top, should be <65
 const n = bt.randIntInRange(1, 20) //Number of stripes on the body
 const rc = bt.randIntInRange(1, 15) //Radius of the circle
 
@@ -53,8 +47,59 @@ drawLines(cT.lines())
 const sT = new bt.Turtle()
 
 for (let i = 1; i <= n; i++) {
-  sT.jump([(i / (n + 1) * 100) + (25/2), 25 / 2])
-  sT.goTo([(i / (n + 1) * (2 * c)) + (125/2 - c), 1 / 5 * r + 25 / 2])
+  sT.jump([(i / (n + 1) * 100) + (25 / 2), 25 / 2])
+  sT.goTo([(i / (n + 1) * (2 * c)) + (125 / 2 - c), 1 / 5 * r + 25 / 2])
 }
 
 drawLines(sT.lines())
+
+//draws random planets on the left side in the background
+for (let i = 0; i < bt.randIntInRange(1, 4); i++) {
+  const pT = new bt.Turtle()
+  let radius = bt.randIntInRange(1, 10)
+  while(radius > (m[0] - r)){
+    radius = bt.randIntInRange(1, 10)
+  }
+  
+  const xPos = bt.randIntInRange(radius, m[0] - r)
+  const yPos = bt.randIntInRange(1 / 5 * r + 25 / 2, 120 - radius)
+
+  pT.jump([xPos, yPos])
+  pT.arc(360, radius)
+
+  drawLines(pT.lines())
+}
+
+//draws random planets on the right side in the background
+for (let i = 0; i < bt.randIntInRange(1, 4); i++) {
+  const pT = new bt.Turtle()
+  let radius = bt.randIntInRange(1, 10)
+  while(120 - radius < (m[0] + r)){
+    radius = bt.randIntInRange(1, 10)
+  }
+  
+  const xPos = bt.randIntInRange(m[0] + r, 120 - radius)
+  const yPos = bt.randIntInRange(1 / 5 * r + 25 / 2, 120 - radius)
+
+  pT.jump([xPos, yPos])
+  pT.arc(360, radius)
+
+  drawLines(pT.lines())
+}
+
+//draws random planets in the top middle in the background
+for (let i = 0; i < bt.randIntInRange(1, 4); i++) {
+  const pT = new bt.Turtle()
+  let radius = bt.randIntInRange(1, 10)
+  while((120 - radius < (m[0] + r)) || ((120 - radius) < (m[1] + r))){
+    radius = bt.randIntInRange(1, 10)
+  }
+  
+  const xPos = bt.randIntInRange(radius, 120 - radius)
+  const yPos = bt.randIntInRange(m[1] + r, 120 - radius)
+
+  pT.jump([xPos, yPos])
+  pT.arc(360, radius)
+
+  drawLines(pT.lines())
+}
