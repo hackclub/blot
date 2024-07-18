5f6789a4f332c95c37c26534c5c6e31a4feb58df
Dhyan Tanna <79247409+D99-1@users.noreply.github.com>
2024-07-18 14:48:24 +1000

    Update index.js

--- art/Burger-Dhyan99/index.js
+++ art/Burger-Dhyan99/index.js
@@ -1,21 +1,17 @@
-setDocDimensions(800, 600);
+setDocDimensions(800, 800);
 
 // Settings
-const seedTop = 3439;
+const seedTop = 3432;
 //
-
-
 const seedBottom = ((seedTop * 63) % 42) + 79;
 
 const myTurtleTop = new bt.Turtle();
-myTurtleTop.jump([600, 375]);
+myTurtleTop.jump([600, 600]);
 myTurtleTop.right(-90);
 myTurtleTop.arc(180, 200);
-myTurtleTop.goTo([600, 375]);
+myTurtleTop.goTo([600, 600]);
 let polylinesTop = myTurtleTop.lines();
-
 bt.scale(polylinesTop, [1.5, 0.8]);
-
 bt.setRandSeed(seedTop);
 
 const sesameSeedsTop = [];
@@ -23,7 +19,7 @@ const numSeedsTop = 35;
 
 for (let i = 0; i < numSeedsTop; i++) {
   const x = bt.randInRange(150, 650);
-  const y = bt.randInRange(425, 500);
+  const y = bt.randInRange(650, 725);
 
   sesameSeedsTop.push([
     [x, y],
@@ -39,9 +35,7 @@ myTurtleBottom.right(90);
 myTurtleBottom.arc(180, 200);
 myTurtleBottom.goTo([200, 225]);
 let polylinesBottom = myTurtleBottom.lines();
-
 bt.scale(polylinesBottom, [1.5, 0.8]);
-
 bt.setRandSeed(seedBottom);
 
 const sesameSeedsBottom = [];
@@ -60,18 +54,30 @@ for (let i = 0; i < numSeedsBottom; i++) {
 }
 
 const pattyTurtle = new bt.Turtle();
-pattyTurtle.jump([115, 390]);
+pattyTurtle.jump([115, 610]);
 pattyTurtle.right(180);
-pattyTurtle.arc(180, 50);
-pattyTurtle.goTo([685, 290]);
-pattyTurtle.arc(180, 50);
-pattyTurtle.goTo([115, 390])
+pattyTurtle.arc(180, 40);
+pattyTurtle.goTo([685, 530]);
+pattyTurtle.arc(180, 40);
+pattyTurtle.goTo([115, 610]);
 let pattyPolylines = pattyTurtle.lines();
+
+const squiggleTurtle = new bt.Turtle();
+squiggleTurtle.jump([115, 480]); 
+squiggleTurtle.right(0); 
+const squiggleDetail = 150;
+const squiggleAmplitude = 33;
+
+for (let i = 0; i < squiggleDetail; i++) {
+  const x = 115 + (i * 4);
+  const y = 480 + squiggleAmplitude * Math.sin(i * Math.PI / 10);
+  squiggleTurtle.goTo([x, y]);
+}
+let squigglePolylines = squiggleTurtle.lines();
 
 drawLines(polylinesTop);
 drawLines(sesameSeedsTop);
-
 drawLines(pattyPolylines);
-
 drawLines(polylinesBottom);
 drawLines(sesameSeedsBottom);
+drawLines(squigglePolylines);
