70425b9e871e3b89e1a3b5c08842114ec44b6f0c
Muhammad Irtaza <irtazanaqvi05@gmail.com>
2024-07-21 17:46:00 +0500

    Randomized sun height and made it dip below horizon!

--- art/SunsetAtSea-Irtaza/index.js
+++ art/SunsetAtSea-Irtaza/index.js
@@ -17,9 +17,38 @@ const boatLines = [];
 const skyLines = [];
 
 const rr = bt.randInRange;
+const sunSize = rr(15, 25);
+const seaLevel = rr(height / 4, height / 2.1);
+const sunCenterX = width / 2;
+const sunHeightOffset = rr(-sunSize, seaLevel - sunSize / 2);
 
+// Sun rays
+const tsRays = new bt.Turtle();
+const rayCount = 50; // Number of rays
+const angleIncrement = 180 / rayCount;
 
+for (let i = 0; i < rayCount; i++) {
+  tsRays.up();
+  tsRays.goTo([sunCenterX, seaLevel + sunHeightOffset + sunSize + sunSize / 4]);
+  tsRays.forward(sunSize - 1);
+  tsRays.left(90);
+  tsRays.down();
+  tsRays.forward(sunSize / 2);
+  tsRays.left(angleIncrement);
+}
+bt.join(raysLines, tsRays.lines());
 
+// Sun
+const tSun = new bt.Turtle();
+tSun.up();
+tSun.goTo([sunCenterX - sunSize + 1, seaLevel + sunHeightOffset + sunSize + sunSize / 4]);
+tSun.down();
+tSun.left(90);
+for (let i = 0; i < 360; i++) {
+  tSun.forward((3 * sunSize) / 180);
+  tSun.right(1);
+}
+bt.join(sunLines, tSun.lines());
 
 // Sea
 const tSea = new bt.Turtle();
@@ -103,7 +132,6 @@ for (let i = 0; i < boatCount; i++) {
   bt.join(finalLines, tBoat.lines());
 }
 
-
 // Drawing the birds
 const birdCount = Math.floor(rr(2, 5));
 const occupiedAreas = [];
@@ -144,12 +172,10 @@ for (let i = 0; i < birdCount; i++) {
   bt.join(birdLines, tBird.lines());
 }
 
-drawLines(seaLines, { fill: "#b5e8d2", stroke: "darkblue" });
 
-//drawLines(boatLines, {fill: "	#dee7d0"});
-drawLines(skyLines, { fill: "	#c9e8d2" });
-
+drawLines(skyLines, { fill: "#c9e8d2" });
 drawLines(sunLines, { stroke: "yellow", fill: "#ffe3ce" });
 drawLines(raysLines, { stroke: "#FFC090" });
+drawLines(seaLines, { fill: "#b5e8d2", stroke: "darkblue" });
 drawLines(birdLines);
-drawLines(finalLines, { fill: "	#dee7d0" });
+drawLines(finalLines, { fill: "#dee7d0" });
