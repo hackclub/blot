8a277731d8c1f4b881c9e5c28b066f56470027a1
Muhammad Irtaza <irtazanaqvi05@gmail.com>
2024-07-21 17:45:21 +0500

    removed old sun code

--- art/SunsetAtSea-Irtaza/index.js
+++ art/SunsetAtSea-Irtaza/index.js
@@ -17,37 +17,9 @@ const boatLines = [];
 const skyLines = [];
 
 const rr = bt.randInRange;
-const sunSize = rr(15, 25);
-const seaLevel = rr(height / 4, height / 2.1);
-const sunCenterX = width / 2;
 
-// Sun rays
-const tsRays = new bt.Turtle();
-const rayCount = 50; // Number of rays
-const angleIncrement = 180 / rayCount;
 
-for (let i = 0; i < rayCount; i++) {
-  tsRays.up();
-  tsRays.goTo([sunCenterX, seaLevel + sunSize + sunSize / 4]);
-  tsRays.forward(sunSize - 1);
-  tsRays.left(90);
-  tsRays.down();
-  tsRays.forward(sunSize / 2);
-  tsRays.left(angleIncrement);
-}
-bt.join(raysLines, tsRays.lines());
 
-// Sun
-const tSun = new bt.Turtle();
-tSun.up();
-tSun.goTo([sunCenterX - sunSize + 1, seaLevel + sunSize + sunSize / 4]);
-tSun.down();
-tSun.left(90);
-for (let i = 0; i < 360; i++) {
-  tSun.forward((3 * sunSize) / 180);
-  tSun.right(1);
-}
-bt.join(sunLines, tSun.lines());
 
 // Sea
 const tSea = new bt.Turtle();
