3002e6c335637e26def1993244e2c562fd701390
Carter Pfaff <79235133+MelonMars@users.noreply.github.com>
2024-07-21 16:46:42 -0400

    Update index.js

--- art/CyberCity-MelonMars/index.js
+++ art/CyberCity-MelonMars/index.js
@@ -7,12 +7,24 @@
 const blockWidth = 50; //Width of a block
 const blockHeight = 70; //Height of a block
 const nBlocks = 5; // Number of blocks on each row and height
-const streetSize = 5; //Thickness of the street
+const streetSize = 20; //Thickness of the street
 const screenHeight = nBlocks * (blockHeight + streetSize);
 const screenWidth = nBlocks * (blockWidth + streetSize);
 const carHeight = 1; //Height of each car
 const carLength = 10; //Length of each car
-const nCars = 5 //Number of cars, similar to number of blocks
+const nCars = 0 //Number of cars, similar to number of blocks
+const stripeFac = 10; //Stripe factor for crosswalks
+const extraGap = 0; //Extra gap between stripes of crosswalk
+const crossWalkSize = 5; //Length of crosswalk
+const laneDashes = 20; //Amount of dashes per lane
+const laneSpacing = 5; //Amount of space between dashes
+const nLanes = 3; //Amount of lanes
+const skyScraperChance = 0.3
+const skyScraperJumps = 5 // How many tiers you want in your tiered skyscraper
+const skyScraperReductionFactor = 0.4;
+const colorCars = false
+const colorBuildings = false
+const colorBackground = false
 setDocDimensions(screenWidth, screenHeight);
 
 function getRandElem(arr) {
@@ -32,6 +44,7 @@ function makeBlock(startPos, bw, bh) {
   blk.forward(bw)
   blk.left(90)
   blk.forward(bh)
+  
   return (blk.lines())
 }
 
@@ -40,21 +53,117 @@ function makeCar(startPos, carL, carH) {
   car.up()
   car.goTo(startPos)
   car.down()
-  let [x, y] = startPos
-  return(
-    [[startPos, [x, y + carH]],
-    [[x, y + carH],[x + carL, y + carH]],
-    [[x + carL, y + carH],[x + carL, y]],
-    [[x + carL, y], startPos]]);
+  car.forward(carL)
+  car.left(90)
+  car.forward(carH)
+  car.left(90)
+  car.forward(carL)
+  car.left(90)
+  car.forward(carH)
+  return (car.lines())  
+
 }
 
-const blocks = []
-for (let blkVert = 0; blkVert < nBlocks; blkVert++) {
-  for (let blkHor = 0; blkHor < nBlocks; blkHor++) {
-    blocks.push(makeBlock([blkHor * (blockWidth + streetSize), blkVert * (blockHeight + streetSize)], blockWidth, blockHeight))
+function drawLanes(bw, bh, streetSize, i, spaceI) {
+  if (spaceI != nLanes) {
+    console.log(spaceI, nLanes);
+    const lw = new bt.Turtle(); 
+    const spacing = streetSize / nLanes;
+    lw.up();
+    i+=1;
+    lw.goTo([(bw*i)+(i*streetSize)-(spaceI*spacing), 0]);
+    lw.left(90);
+    lw.down();
+    for (let i=0; i<laneDashes; i++) {
+      lw.forward((screenHeight/laneDashes)-laneSpacing);
+      lw.up();
+      lw.forward(laneSpacing);
+      lw.down();
+    }
+    lw.up()
+    lw.goTo([0, (bh*i)+(i*streetSize)-(spaceI*spacing)]);
+    lw.down();
+    lw.right(90);
+    for (let i=0; i<laneDashes; i++) {
+      lw.forward((screenWidth/laneDashes)-laneSpacing);
+      lw.up();
+      lw.forward(laneSpacing);
+      lw.down();
+    }
+    lw.up();
+    return lw.lines();
   }
 }
 
+function drawCrosswalks(startPos, bw, bh, streetSize) {
+  const cw = new bt.Turtle();
+  const crosswalkWidth = streetSize;
+  const stripeWidth = crosswalkWidth / stripeFac;
+  const gapWidth = stripeWidth; 
+
+  for (let i = 0; i <= bw; i += bw + streetSize) {
+    for (let j = 0; j < crosswalkWidth; j += stripeWidth + gapWidth) {
+      cw.up();
+      cw.goTo([startPos[0] + i, startPos[1] - j]);
+      cw.down();
+      cw.forward(crossWalkSize);
+      cw.up();
+      cw.forward(bw - (2 * crossWalkSize));
+      cw.down();
+      cw.forward(crossWalkSize);
+    }
+  }
+
+  const verticalPositions = [0, bw + (0.75 * streetSize)];
+  for (let pos of verticalPositions) {
+    for (let l = 0; l < crosswalkWidth; l += stripeWidth + gapWidth) {
+      cw.up();
+      cw.goTo([startPos[0] - l, startPos[1] + pos]);
+      cw.down();
+      cw.left(90);
+      cw.forward(crossWalkSize);
+      cw.right(90);
+    }
+  }
+
+
+  return cw.lines();
+}
+const crosswalks = []
+const blocks = []
+const lanes = []
+for (let blkVert = 0; blkVert < nBlocks; blkVert++) {
+  for (let blkHor = 0; blkHor < nBlocks; blkHor++) {
+    let startPos = [blkHor * (blockWidth + streetSize), blkVert * (blockHeight + streetSize)]
+    if (bt.randInRange(0, 1) < skyScraperChance) {
+      let startPos = [blkHor * (blockWidth + streetSize), blkVert * (blockHeight + streetSize)];
+      let currentW = blockWidth;
+      let currentH = blockHeight;
+
+      for (let jump = 0; jump < skyScraperJumps; jump++) {
+        blocks.push(makeBlock(startPos, currentW, currentH));
+
+        const wRed = currentW * skyScraperReductionFactor;
+        const hRed = currentH * skyScraperReductionFactor;
+
+        currentW -= wRed;
+        currentH -= hRed;
+
+        startPos[0] += wRed / 2;
+        startPos[1] += hRed / 2;
+      }
+    } else {
+      blocks.push(makeBlock([blkHor * (blockWidth + streetSize), blkVert * (blockHeight + streetSize)], blockWidth, blockHeight));
+    }
+    crosswalks.push(drawCrosswalks(startPos, blockWidth, blockHeight, streetSize));
+  }
+}
+
+for (let i = 1; i < nLanes; i++) {
+  for (let laneI = 0; laneI < nBlocks; laneI++) {
+    lanes.push(drawLanes(blockWidth, blockHeight, streetSize, laneI, i));
+  }
+}
 function insideBlock(x, y) {
   const colI = Math.floor(x / (blockWidth + streetSize))
   const rowI = Math.floor(y / (blockHeight + streetSize))
@@ -109,8 +218,6 @@ const vertCarPts = []
 const horCarPts = []
 for (let pt of ptsOutBlk) {
   let [x, y] = pt
-  console.log("x: ", x)
-  console.log("y: ", y)
   if (blkYs.includes(y)) {
     vertCarPts.push([x, y])
   } else {
@@ -142,9 +249,37 @@ border.left(90)
 border.forward(screenWidth)
 border.left(90)
 border.forward(screenHeight)
-drawLines(border.lines(), { "fill": getRandElem(blockCols) })
+if (colorBackground) {
+  drawLines(border.lines(), { "fill": getRandElem(Cols) })
+} else {
+  drawLines(border.lines())
+}
 
-blocks.push(...cars)
-for (const block of blocks) {
-  drawLines(block, { "fill": getRandElem(blockCols) })
+
+for (const crosswalk of crosswalks) {
+  drawLines(crosswalk, {"stroke": "white"});
+}
+
+if (colorBuildings) {
+  for (const block of blocks) {
+    drawLines(block, { "fill": getRandElem(Cols) })
+  }
+} else {
+  for (const block of blocks) {
+    drawLines(block)
+  }
+}
+
+if (colorCars) {
+  for (const car of cars) {
+    drawLines(car, { "fill": getRandElem(Cols) })
+  }
+  }else {
+  for (const car of cars) {
+    drawLines(car)
+  }
+}
+
+for (const lane of lanes) {
+  drawLines(lane, {"fill": "white"});
 }
