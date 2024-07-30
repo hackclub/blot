eacaece13350faa7e7d9933bb965225ba6586fef
asherlief <avi+asher@avibryant.com>
2024-07-29 19:51:05 -0700

    Update index.js

--- art/warpspeed-asherlief/index.js
+++ art/warpspeed-asherlief/index.js
@@ -3,11 +3,12 @@
 @author: asherlief
 @snapshot: center.png
 */
-
+const t = new bt.Turtle()
+const position = t.pos
 const centerX = 62.50;
 const centerY = 62.5;
-const t = new bt.Turtle()
 t.up
+
 function draw(x, y) {
   let x2 = x * 2 - centerX
   let y2 = y * 2 - centerY
@@ -48,31 +49,72 @@ while (i < 1500) {
 }
 t.down
 t.setAngle(90)
-t.jump([1,20])
-t.arc(-180,61.5)
+t.jump([1, 20])
+t.arc(-180, 61.5)
 
 t.setAngle(90)
-t.jump([12.5,20])
-t.arc(-180,50)
+t.jump([12.5, 20])
+t.arc(-180, 50)
 
 t.setAngle(90)
-t.jump([25,20])
-t.arc(-180,37.5)
+t.jump([25, 20])
+t.arc(-180, 37.5)
 
 t.setAngle(90)
-t.jump([37,20])
-t.arc(-180,25)
+t.jump([37, 20])
+t.arc(-180, 25)
 
-t.jump([44,38])
+t.jump([44, 38])
 t.setAngle(135)
 t.forward(35)
 
-t.jump([62.5,45])
+t.jump([62.5, 45])
 t.setAngle(90)
 t.forward(36)
 
-t.jump([81,38])
+t.jump([81, 38])
 t.setAngle(45)
 t.forward(35)
 
-drawLines(bt.offset(t.lines(),1),["fill"])
+drawLines(bt.offset(t.lines(), 1), ["fill"])
+t.setAngle(90)
+i = 0
+let buttonCount = Math.round(bt.randInRange(10, 20))
+while (i < buttonCount) {
+  let x = 0
+  while (x < 1) {
+    t.jump([Math.round(bt.randInRange(1, 24)) * 5, Math.round(bt.randInRange(1, 24)) * 5])
+    if ((t.pos[1] < 14) || (t.pos[1] > 87.5)) {
+      if ((t.pos[0] < 57.5) || (t.pos[0] > 72.5)) {
+        x = 1
+      }
+    }
+  }
+  t.forward(5)
+  t.left(90)
+  t.forward(5)
+  t.left(90)
+  t.forward(5)
+  t.left(90)
+  t.forward(5)
+  t.left(90)
+  i += 1
+}
+
+t.jump([57.5, 3])
+t.forward(14)
+t.right(90)
+t.forward(10)
+t.right(90)
+t.forward(14)
+t.right(90)
+t.forward(10)
+t.right(90)
+
+i = 0
+while (i < 100) {
+t.jump([(bt.randInRange(57.5, 67.5)),(bt.randInRange(3, 17))])
+t.arc(360,0.05)
+i += 1
+}
+drawLines(t.lines())
