2978e141c7a747fc793ecf9b5015398ea2cb4872
TMShader <github@tmshader.me>
2024-08-05 17:58:48 +0200

    Add some controllable randomness

--- art/Waves-TMShader/index.js
+++ art/Waves-TMShader/index.js
@@ -5,9 +5,11 @@
 */
 
 /* Variables */
-const lineNum = 50;
+const lineNum = 100;
 const margin = 5;
-const break_x = 4; /* 3-6 gives reasonable results */
+const break_x = 4; /* 3-6 give reasonable results */
+const random_start = 1;
+const random_break = 1;
 /* --- */
 
 const width = 200;
@@ -17,8 +19,14 @@ setDocDimensions(width, height);
 
 const breakAvg = (breakx, y) => {
   const distance = Math.min(y - margin, height - y - margin) / (height - margin * 2);
-  return [breakx[0], (breakx[1] * distance) + (y * (1 - distance))];
+
+  return [
+    breakx[0] + bt.randInRange(-random_break, random_break),
+    marginClamp((breakx[1] * distance) + (y * (1 - distance)) + bt.randInRange(-random_break, random_break))
+  ];
 }
+
+const marginClamp = (coord) => Math.max(margin, Math.min(height - margin, coord));
 
 const lines = [];
 const step = (height - margin * 2) / lineNum;
@@ -28,8 +36,9 @@ const break2 = [(width / break_x) * (break_x - 1), height - margin]
 
 for (let i = 0; i <= lineNum; i++) {
   let y = i * step + margin;
+
   lines.push(bt.catmullRom([
-    [margin, y], breakAvg(break1, y), breakAvg(break2, y), [width - margin, y]
+    [margin, marginClamp(y + bt.randInRange(-random_start, random_start))], breakAvg(break1, y), breakAvg(break2, y), [width - margin, marginClamp(y + bt.randInRange(-random_start, random_start))]
   ]))
 }
 
