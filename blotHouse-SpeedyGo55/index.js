2789f9411aaa96927eeee460aaf36e158dd7991f
SpeedyGo55 <83915114+SpeedyGo55@users.noreply.github.com>
2024-08-16 08:51:23 +0200

    Update index.js

--- blotHouse-SpeedyGo55/index.js
+++ blotHouse-SpeedyGo55/index.js
@@ -5,7 +5,7 @@
 */
 const width = 125;
 const height = 125;
-const randSeed = 1719;
+const randSeed = 2094;
 
 bt.setRandSeed(randSeed)
 
@@ -53,6 +53,7 @@ t.forward(house_width)
 t.right(90)
 t.forward(house_height)
 t.right(90)
+
 t.up()
 t.forward(door_distance)
 t.right(90)
@@ -69,7 +70,6 @@ t.forward(2)
 t.right(90)
 t.forward(door_height / 2)
 
-
 drawLines(t.lines())
 
 t = new bt.Turtle()
@@ -84,5 +84,61 @@ for (let i = 1; i < cnt_rays; i++) {
   t.right(90)
 }
 t.arc(90 / cnt_rays, radius)
+
+drawLines(t.lines())
+
+t = new bt.Turtle()
+let tree_count = bt.randIntInRange(1, 3)
+for (let i = 0; i < tree_count; i++) {
+  let tree_x = bt.randInRange(20,105)
+  let tree_y = start + 20 * bt.noise(tree_x)
+
+  t.jump([tree_x, tree_y])
+  t.down()
+
+  t.left(90)
+  t.forward(10 + 20 * bt.rand())
+  t.right(90)
+
+  let choice = bt.randIntInRange(0, 1);
+  switch (choice) {
+    case 0:
+      t.forward(5)
+      t.right(120)
+      t.forward(10)
+      t.right(120)
+      t.forward(10)
+      t.right(120)
+      t.forward(5)
+
+      t.up()
+    case 1:
+      t.arc(360,5 + 10 * bt.rand())
+    default:
+      break;
+  }
+}
+
+  drawLines(t.lines())
+
+t = new bt.Turtle()
+let cloud_x = bt.randInRange(20, 80) 
+let cloud_y = bt.randInRange(95, 115) 
+
+let cloud_radius = 5 * bt.rand() 
+let cloud_length = bt.randIntInRange(3, 6)
+
+t.jump([cloud_x, cloud_y])
+t.down()
+
+for (let i = 0; i < cloud_length; i++) {
+  t.arc(360, cloud_radius)
+  t.up()
+  t.forward(cloud_radius)
+  t.left(90)
+  t.forward(bt.randInRange(-2,2))
+  t.right(90)
+  t.down()
+}
 
 drawLines(t.lines())
