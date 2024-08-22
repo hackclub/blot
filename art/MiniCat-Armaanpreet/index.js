661e9bd8c310088e8c777fb2c99213a73d07a004
4n0nym0u5-3y3 <anonymousss.3y3@gmail.com>
2024-08-22 14:08:28 +0200

    Update index.js

--- art/MiniCat-Armaanpreet/index.js
+++ art/MiniCat-Armaanpreet/index.js
@@ -78,8 +78,14 @@ function drawCat() {
   // Whiskers
   const whiskerLength = randInRange(10, 20);
   for (let i = 0; i < 3; i++) {
-    lines.push([[noseCenter[0] - noseRadius, noseCenter[1] - i * 5], [noseCenter[0] - noseRadius - whiskerLength, noseCenter[1] - i * 5]]);
-    lines.push([[noseCenter[0] + noseRadius, noseCenter[1] - i * 5], [noseCenter[0] + noseRadius + whiskerLength, noseCenter[1] - i * 5]]);
+    lines.push([
+      [noseCenter[0] - noseRadius, noseCenter[1] - i * 5],
+      [noseCenter[0] - noseRadius - whiskerLength, noseCenter[1] - i * 5]
+    ]);
+    lines.push([
+      [noseCenter[0] + noseRadius, noseCenter[1] - i * 5],
+      [noseCenter[0] + noseRadius + whiskerLength, noseCenter[1] - i * 5]
+    ]);
   }
 
   // Paws
@@ -88,6 +94,50 @@ function drawCat() {
   const pawRightCenter = [bodyCenter[0] + bodyRadius / 2, bodyCenter[1] - bodyRadius / 2];
   lines.push(...circle(pawLeftCenter, pawRadius));
   lines.push(...circle(pawRightCenter, pawRadius));
+
+  // Hands
+  const handRadius = pawRadius * 0.75; // Smaller than paws
+  const handLeftCenter = [bodyCenter[0] - bodyRadius / 2, bodyCenter[1] + bodyRadius / 4];
+  const handRightCenter = [bodyCenter[0] + bodyRadius / 2, bodyCenter[1] + bodyRadius / 4];
+  lines.push(...circle(handLeftCenter, handRadius));
+  lines.push(...circle(handRightCenter, handRadius));
+
+  // Cute Hand Details
+  function addCuteFingers(center, radius, direction) {
+    const fingerLength = radius * 0.6;
+    const fingerRadius = radius * 0.2;
+    const offsets = [
+      [-radius * 0.4, -radius * 0.4],
+      [0, -radius * 0.6],
+      [radius * 0.4, -radius * 0.4]
+    ];
+    offsets.forEach(([dx, dy]) => {
+      const fingerCenter = [center[0] + dx, center[1] + dy];
+      lines.push(...circle(fingerCenter, fingerRadius));
+    });
+  }
+
+  addCuteFingers(handLeftCenter, handRadius, 'left');
+  addCuteFingers(handRightCenter, handRadius, 'right');
+
+  // Bell
+  const bellRadius = randInRange(5, 10);
+  const bellCenter = [bodyCenter[0], bodyCenter[1] + bodyRadius / 4 + bellRadius / 4];
+  lines.push(...circle(bellCenter, bellRadius));
+
+  // Bell clapper (small line inside the bell)
+  lines.push([
+    [bellCenter[0], bellCenter[1] + bellRadius / 2],
+    [bellCenter[0], bellCenter[1] + bellRadius]
+  ]);
+
+  // Bell attachment (small loop on top of the bell)
+  const bellAttachment = [
+    [bellCenter[0] - bellRadius / 3, bellCenter[1] - bellRadius / 2],
+    [bellCenter[0], bellCenter[1] - bellRadius],
+    [bellCenter[0] + bellRadius / 3, bellCenter[1] - bellRadius / 2]
+  ];
+  lines.push(bellAttachment);
 
   drawLines(lines);
 }
@@ -114,3 +164,4 @@ function circle(center, radius) {
 }
 
 drawCat();
+
