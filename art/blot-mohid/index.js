54e236df693e438312c284f094ab51f0ecf9f38d
M0HID <91326009+M0HID@users.noreply.github.com>
2024-08-24 14:45:39 +0100

    Update index.js

--- art/blot-mohid/index.js
+++ art/blot-mohid/index.js
@@ -1,8 +1,3 @@
-// welcome to blot!
-
-// check out this guide to learn how to program in blot
-// https://blot.hackclub.com/editor?guide=start
-
 const width = 125;
 const height = 125;
 
@@ -13,168 +8,272 @@ const finalLines = [];
 
 // create a polyline
 
-const posx = width/2
-const posy = height/2
+const posx = width / 2;
+const posy = height / 2;
+
+function ellipse(left, right, top, bottom, numPoints = 100) {
+  const x_center = (left + right) / 2;
+  const y_center = (top + bottom) / 2;
+  const r_x = (right - left) / 2;
+  const r_y = (top - bottom) / 2;
+
+  const points = [];
+  for (let i = 0; i <= numPoints; i++) {
+    const theta = (i / numPoints) * 2 * Math.PI;
+    const x = x_center + r_x * Math.cos(theta);
+    const y = y_center + r_y * Math.sin(theta);
+    points.push([x, y]);
+  }
+
+  drawLines([points]);
+}
+
 const LSfrontSquare = [
-  [posx-50,posy-2],
-  [posx-50,posy+8],
-  [posx-42,posy+7],
-  [posx-42,posy-3],
-  [posx-50,posy-2],
+  [posx - 50, posy - 2],
+  [posx - 50, posy + 8],
+  [posx - 42, posy + 7],
+  [posx - 42, posy - 3],
+  [posx - 50, posy - 2],
 ];
 const LSbackSquare = bt.copy(LSfrontSquare);
-bt.translate([LSbackSquare],[5,5])
+bt.translate([LSbackSquare], [5, 5]);
 const LSconnectingLines = [
-  [posx-50,posy+8],
-  [posx-45,posy+13],
-  [posx-37,posy+12],
-  [posx-37,posy+2],
-  [posx-42,posy-3],
-  [posx-42,posy+7],
-  [posx-37,posy+12],
-  [posx-45,posy+13],
-  [posx-50,posy+8],
-]
+  [posx - 50, posy + 8],
+  [posx - 45, posy + 13],
+  [posx - 37, posy + 12],
+  [posx - 37, posy + 2],
+  [posx - 42, posy - 3],
+  [posx - 42, posy + 7],
+  [posx - 37, posy + 12],
+  [posx - 45, posy + 13],
+  [posx - 50, posy + 8],
+];
 
 const LSrotor = [
-  [posx-44.5,posy+15],
-  [posx-44.5,posy+10],
-  [posx-43.5,posy+10],
-  [posx-43.5,posy+15],
-  [posx-44.5,posy+15],  
-]
+  [posx - 44.5, posy + 15],
+  [posx - 44.5, posy + 10],
+  [posx - 43.5, posy + 10],
+  [posx - 43.5, posy + 15],
+  [posx - 44.5, posy + 15],
+];
 finalLines.push(LSrotor);
 
 const LSstand = [
-  [posx-42,posy+7],
-  [posx-45,posy-11],
-  [posx-42.5,posy-11],
-  [posx-40,posy-9],
-  [posx-38.6,posy-3],
-  [posx-33,posy+3],
-  [posx-37,posy+8.5],
-  [posx-39,posy+6.5],
-  [posx-42,posy+7],
-  [posx-45,posy-11],
-  [posx-42.5,posy-11],
-  [posx-39,posy+6.5],
-
-]
+  [posx - 42, posy + 7],
+  [posx - 45, posy - 11],
+  [posx - 42.5, posy - 11],
+  [posx - 40, posy - 9],
+  [posx - 38.6, posy - 3],
+  [posx - 33, posy + 3],
+  [posx - 37, posy + 8.5],
+  [posx - 39, posy + 6.5],
+  [posx - 42, posy + 7],
+  [posx - 45, posy - 11],
+  [posx - 42.5, posy - 11],
+  [posx - 39, posy + 6.5],
+];
 
 const LSmount = [
-  // [posx-38,posy+11],
-  // [posx-29,posy+10.2],
-  // [posx-32,posy+7],
-  // [posx-41,posy+8],
-  // [posx-32,posy+7],
-  // [posx-32,posy-3],
-  // [posx-41,posy-2],
-  // [posx-32,posy-3],
-  // [posx-29,posy+0.7],
-  // [posx-29,posy+10.2],
-  // [posx-38,posy+11],
-  // [posx-41,posy+8],
-  // [posx-41,posy-2],
-  [posx-41,posy+8],
-  [posx-38,posy+11],
-  [posx-29,posy+10.2],
-  [posx-29,posy+0.7],
-  [posx-32,posy-3],
-  [posx-41,posy-2],
-  [posx-41,posy+8],
-  [posx-32,posy+7],
-  [posx-32,posy-3],
-  [posx-32,posy+7],
-  [posx-29,posy+10.2],
-  [posx-32,posy+7],
-]
+  [posx - 41, posy + 8],
+  [posx - 38, posy + 11],
+  [posx - 29, posy + 10.2],
+  [posx - 29, posy + 0.7],
+  [posx - 32, posy - 3],
+  [posx - 41, posy - 2],
+  [posx - 41, posy + 8],
+  [posx - 32, posy + 7],
+  [posx - 32, posy - 3],
+  [posx - 32, posy + 7],
+  [posx - 29, posy + 10.2],
+  [posx - 32, posy + 7],
+];
 
-
-finalLines.push(...bt.cover(bt.cover(bt.cover([LSconnectingLines], bt.cover([LSbackSquare], [LSfrontSquare])), [LSstand]), [LSmount]));
+finalLines.push(
+  ...bt.cover(
+    bt.cover(bt.cover([LSconnectingLines], bt.cover([LSbackSquare], [LSfrontSquare])), [LSstand]),
+    [LSmount]
+  )
+);
 finalLines.push(...bt.cover(bt.cover([LSfrontSquare], [LSstand]), [LSmount]));
-finalLines.push(...bt.cover(bt.cover(bt.cover([LSconnectingLines], [LSstand]), [LSmount]), [LSmount]));
-finalLines.push(...bt.cover([LSstand],[LSmount]))
+finalLines.push(
+  ...bt.cover(bt.cover(bt.cover([LSconnectingLines], [LSstand]), [LSmount]), [LSmount])
+);
+
 
 const xrail = [
-  [posx+21,posy+1.4],
-  [posx-30,posy+7],
-  [posx-31.2,posy+6],
-  [posx+20,posy+0.4],
-  [posx-31.2,posy+6],
-  [posx-31.2,posy+4],
-  [posx+20,posy-1.4],
-  [posx-31.2,posy+4],
-  [posx-31.2,posy+3],
-  [posx+20,posy-2.4],
-  [posx-31.2,posy+3],
-  [posx-31.2,posy+1],
-  [posx+20,posy-4.4],
-  [posx+20,posy+0.4],
-  [posx+21,posy+1.4],
+  [posx + 21, posy + 1.4],
+  [posx - 30, posy + 7],
+  [posx - 31.2, posy + 6],
+  [posx + 20, posy + 0.4],
+  [posx - 31.2, posy + 6],
+  [posx - 31.2, posy + 4],
+  [posx + 20, posy - 1.4],
+  [posx - 31.2, posy + 4],
+  [posx - 31.2, posy + 3],
+  [posx + 20, posy - 2.4],
+  [posx - 31.2, posy + 3],
+  [posx - 31.2, posy + 1],
+  [posx + 20, posy - 4.4],
+  [posx + 20, posy + 0.4],
+  [posx + 21, posy + 1.4],
+];
 
-]
 const RSmount = [
-
-  [posx+21,posy+4.5],
-  [posx+19.5,posy+0.5],
-  [posx+19.5,posy+-6.7],
-  [posx+25,posy-7.3],
-  [posx+28,posy+3.6],
-  [posx+21,posy+4.5],
-  [posx+19.5,posy+0.5],
-  [posx+25.5,posy+-0.2],
-]
+  [posx + 22.5, posy + 4.2],
+  [posx + 19.5, posy + 0.5],
+  [posx + 19.5, posy - 6.7],
+  [posx + 25, posy - 7.3],
+  [posx + 28, posy + 3.6],
+  [posx + 22.5, posy + 4.2],
+  [posx + 19.5, posy + 0.5],
+  [posx + 25.5, posy - 0.2],
+];
 
 const RSstand = [
-  [posx+24.7,posy-1.2],
-  [posx+21,posy-20],
-  [posx+24,posy-20],
-  [posx+27.7,posy+-1.4],
-  [posx+27.7,posy-10.8],
-  [posx+24,posy-20],
-  [posx+27.7,posy-10.8],
-  [posx+37.2,posy-12.3],
-  [posx+37.7,posy-2.5],
-  [posx+39.7,posy+3.7],
-  [posx+27.6,posy+4.9],
-  [posx+24.7,posy-1.2],
-  [posx+37.7,posy-2.5],
-]
+  [posx + 24.7, posy - 1.2],
+  [posx + 21, posy - 20],
+  [posx + 24, posy - 20],
+  [posx + 27.7, posy - 1.4],
+  [posx + 27.7, posy - 10.8],
+  [posx + 24, posy - 20],
+  [posx + 27.7, posy - 10.8],
+  [posx + 37.2, posy - 12.3],
+  [posx + 37.7, posy - 2.5],
+  [posx + 39.7, posy + 3.7],
+  [posx + 27.6, posy + 4.9],
+  [posx + 24.7, posy - 1.2],
+  [posx + 37.7, posy - 2.5],
+];
 
 const RSrotor = [
-  [posx+34.5,posy+15],
-  [posx+34.5,posy+10],
-  [posx+33.5,posy+10],
-  [posx+33.5,posy+15],
-  [posx+34.5,posy+15],  
-]
-finalLines.push(RSrotor)
-finalLines.push(RSstand)
-finalLines.push(...bt.cover([RSmount],[RSstand]))
-finalLines.push(...bt.cover([LSmount],[xrail]))
-finalLines.push(...bt.cover([xrail],[RSmount]))
+  [posx + 33.5, posy + 7],
+  [posx + 33.5, posy + 2],
+  [posx + 32.5, posy + 2],
+  [posx + 32.5, posy + 7],
+  [posx + 33.5, posy + 7],
+];
+finalLines.push(RSrotor);
+finalLines.push(RSstand);
 
 
 
 
+const pos = 55; // (-23)-(65)
+const yslide = -21; // (-15)-(-90)
+
+const xStart = xrail[0][0];
+const yStart = xrail[0][1];
+const xEnd = xrail[1][0];
+const yEnd = xrail[1][1];
+
+const xPos = xStart + (xEnd - xStart) * (pos / 100);
+const yPos = yStart + (yEnd - yStart) * (pos / 100);
+
+const yRailGradient = (22.5 - 3.8) / (-10.2 + 26.9);
+
+const yslideXOffset = (16.7) * (yslide / 100);
+const yslideYOffset = yRailGradient * yslideXOffset;
 
 const yrail = [
-  [posx+-10.2,posy+22.5],
-  [posx+-26.9,posy+3.8],
-  [posx+-25.1,posy+3.5],
-  [posx+-8.4,posy+22.2],
-  [posx+-25.1,posy+3.5],
-  [posx+-22.8,posy+3.2],
-  [posx+-6.1,posy+22.2],
-  [posx+-22.8,posy+3.2],
-  [posx+-21.1,posy+3.1],
-  [posx-4.4,posy+22.4],
-  [posx+-21.1,posy+3.1],
-  [posx+-21.1,posy+-1.9],
-  [posx-4.4,posy+17.9],
-  
+  [xPos + yslideXOffset, yPos + 22.5 + yslideYOffset],
+  [xPos - 16.7 + yslideXOffset, yPos + 3.8 + yslideYOffset],
+  [xPos - 15.1 + yslideXOffset, yPos + 3.5 + yslideYOffset],
+  [xPos + 1.6 + yslideXOffset, yPos + 22.2 + yslideYOffset],
+  [xPos - 15.1 + yslideXOffset, yPos + 3.5 + yslideYOffset],
+  [xPos - 12.8 + yslideXOffset, yPos + 3.2 + yslideYOffset],
+  [xPos + 3.9 + yslideXOffset, yPos + 22.2 + yslideYOffset],
+  [xPos - 12.8 + yslideXOffset, yPos + 3.2 + yslideYOffset],
+  [xPos - 11.1 + yslideXOffset, yPos + 3.1 + yslideYOffset],
+  [xPos + 5.6 + yslideXOffset, yPos + 22.4 + yslideYOffset],
+  [xPos - 11.1 + yslideXOffset, yPos + 3.1 + yslideYOffset],
+  [xPos - 11.1 + yslideXOffset, yPos - 1.9 + yslideYOffset],
+  [xPos + 5.6 + yslideXOffset, yPos + 17.9 + yslideYOffset],
+];
+
+const yclip = [
+  [xPos + 5.6 + yslideXOffset, yPos + 17.9 + yslideYOffset],
+  [xPos +5.6 + yslideXOffset, yPos + 22.2 + yslideYOffset],
+  [xPos + 4.9 + yslideXOffset, yPos + 21.6 + yslideYOffset],
+  [xPos + -0.3 + yslideXOffset, yPos + 22.0 + yslideYOffset],
+  [xPos + -0.3 + yslideXOffset, yPos + 23.2 + yslideYOffset],
+  [xPos + 1.4 + yslideXOffset, yPos + 24.8 + yslideYOffset],
+  [xPos + 6.4 + yslideXOffset, yPos + 24.5 + yslideYOffset],
+  [xPos + 6.4 + yslideXOffset, yPos + 16.8 + yslideYOffset],
+  [xPos + 5.6 + yslideXOffset, yPos + 15.7 + yslideYOffset],
+  [xPos +5.6 + yslideXOffset, yPos + 22.2 + yslideYOffset],
+  [xPos + 4.9 + yslideXOffset, yPos + 21.6 + yslideYOffset],
+  [xPos + 4.9 + yslideXOffset, yPos + 22.8 + yslideYOffset],
+  [xPos + -0.3 + yslideXOffset, yPos + 23.2 + yslideYOffset],
+  [xPos + 4.9 + yslideXOffset, yPos + 22.8 + yslideYOffset],
+  [xPos + 6.4 + yslideXOffset, yPos + 24.5 + yslideYOffset],
+];
+
+const ymount = [
+  [xPos + -18.7 + yslideXOffset, yPos + 4.0 + yslideYOffset],
+  [xPos + -11.0 + yslideXOffset, yPos + 3.0 + yslideYOffset],
+  [xPos + -11.0 + yslideXOffset, yPos + -10.8 + yslideYOffset],
+  [xPos + -14.5 + yslideXOffset, yPos + -15.5 + yslideYOffset],
+  [xPos + -22.2 + yslideXOffset, yPos + -14.5 + yslideYOffset],
+  [xPos + -22.2 + yslideXOffset, yPos + 1.0 + yslideYOffset],
+  [xPos + -18.7 + yslideXOffset, yPos + 4.0 + yslideYOffset],
+  [xPos + -22.2 + yslideXOffset, yPos + 1.0 + yslideYOffset],
+  [xPos + -14.2 + yslideXOffset, yPos + yslideYOffset],
+  [xPos + -11.0 + yslideXOffset, yPos + 3.0 + yslideYOffset],
+  [xPos + -14.2 + yslideXOffset, yPos + yslideYOffset],
+  [xPos + -14.5 + yslideXOffset, yPos + -15.5 + yslideYOffset],
+  [xPos + -22.2 + yslideXOffset, yPos + -14.5 + yslideYOffset],
+  [xPos + -22.2 + yslideXOffset, yPos + 1.0 + yslideYOffset],
 ]
 
-finalLines.push(yrail)
+const ypenclip = [
+  [xPos + -30.5 + yslideXOffset, yPos + -7.9 + yslideYOffset],
+  [xPos + -13.4 + yslideXOffset, yPos + -10.7 + yslideYOffset],
+  [xPos + -13.4 + yslideXOffset, yPos + -25.4 + yslideYOffset],
+  [xPos + -30.5 + yslideXOffset, yPos + -22.6 + yslideYOffset],
+  [xPos + -31.1 + yslideXOffset, yPos + -9.0 + yslideYOffset],
+  [xPos + -30.5 + yslideXOffset, yPos + -7.9 + yslideYOffset],
+  [xPos + -31.1 + yslideXOffset, yPos + -9.0 + yslideYOffset],
+  [xPos + -13.8 + yslideXOffset, yPos + -11.7 + yslideYOffset],
+  [xPos + -13.8 + yslideXOffset, yPos + -25.2 + yslideYOffset],
+  [xPos + -13.8 + yslideXOffset, yPos + -11.7 + yslideYOffset],
+  [xPos + -13.4 + yslideXOffset, yPos + -10.7 + yslideYOffset],
+  [xPos + -13.4 + yslideXOffset, yPos + -25.4 + yslideYOffset],
+]
+
+const pen = [
+  [xPos + -25.6 + yslideXOffset, yPos + 11.8 + yslideYOffset],
+  [xPos + -24.6 + yslideXOffset, yPos + -31.9 + yslideYOffset],
+  [xPos + -23.0 + yslideXOffset, yPos + -34.5 + yslideYOffset],
+  [xPos + -21.5 + yslideXOffset, yPos + -31.5 + yslideYOffset],
+  [xPos + -22.9 + yslideXOffset, yPos + 11.7 + yslideYOffset],
+]
+
+ellipse(xPos + -25.6 + yslideXOffset, xPos + -22.9 + yslideXOffset, yPos + 12.3 + yslideYOffset, yPos + 11.3 + yslideYOffset);
+
+// the yrail wheel thingies x = 5.9 y = 2
+ellipse(xPos + 0.0, xPos + -6.3, yPos + 3.7, yPos + 5.7);
+ellipse(xPos + 0.0, xPos + -6.3, yPos + 6.6, yPos + 8.3);
+ellipse(xPos + -2.2, xPos + 3.7, yPos + 11.7, yPos + 13.7);
+ellipse(xPos + -2.2, xPos + 3.7, yPos + 9.7, yPos + 11.7);
+ellipse(xPos + -11.9, xPos + -6.9, yPos + 14.7, yPos + 12.4);
+ellipse(xPos + -11.9, xPos + -6.9, yPos + 12.7, yPos + 10.4);
+ellipse(xPos + -16.8, xPos + -11.8, yPos + 7.3, yPos + 9.2);
+ellipse(xPos + -16.8, xPos + -11.8, yPos + 5.3, yPos + 7.2);
+
+
+
+finalLines.push(pen)
+finalLines.push(...bt.cover([ypenclip],[pen]))
+finalLines.push(...bt.cover(bt.cover([ymount],[pen]), [ypenclip]))
+
+finalLines.push(...bt.cover(bt.cover(bt.cover(bt.cover([xrail], [RSmount]), [yrail]), [ymount]), [pen]));
+finalLines.push(...bt.cover([RSmount], [RSstand]));
+
+finalLines.push(...bt.cover(bt.cover(bt.cover(bt.cover([LSmount], [xrail]), [yrail]), [ymount]),[pen]));
+
+finalLines.push(...bt.cover(bt.cover(bt.cover(bt.cover(bt.cover([LSstand], [LSmount]), [yrail]), [ymount]), [pen]), [ypenclip]));
+finalLines.push(...bt.cover([yclip],[xrail]));
+finalLines.push(...bt.cover(bt.cover([yrail], [yclip]), [RSmount]));
+
 // draw it
 drawLines(finalLines);
