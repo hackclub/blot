7f527235ef8fc48257e9615af6e751c618aa1a34
Pratyush Roy <pratyushroy.whj@gmail.com>
2024-08-25 01:23:40 +0530

    New design and images.

--- art/LissajousCurve-Pratyush/index.js
+++ art/LissajousCurve-Pratyush/index.js
@@ -1,42 +1,42 @@
 /*
-@title: LissajousCurve
+@title: LissajousCurveWithSquares
 @author: Pratyush Roy
+@snapshot: snapshot1.png
 */
 
 const { Turtle, randInRange } = bt;
-const TILT = -31
 
-function drawComplexLissajous(t, a, b, d, n, iterations) {
-  let step = 2 * Math.PI / n;
+function drawSquare(t, x, y, size) {
+  t.jump([x - size / 2, y - size / 2]);
+  t.down();
+  
+  for (let i = 0; i < 4; i++) {
+    t.forward(size);
+    t.left(90);
+  }
+  
+  t.up();
+}
+
+function draw(t, a, b, d, n, iterations) {
+  let step = 4 * Math.PI / n;
 
   for (let j = 0; j < iterations; j++) {
     let px = 0, py = 0;
-    let ampX = 30 + 50 * Math.sin(j * 0.4);
-    let ampY = -3 + 50 * Math.cos(j * 0.9);
-    let offset = j * 2;
+    let offset = j * -1;
 
     for (let i = 0; i <= n; i++) {
       let v = i * step;
-      let x = ampX * Math.sin(a * v + d + j / 3) + bt.randInRange(-1, -1);
-      let y = ampY * Math.sin(b * v + j / 4) + bt.randInRange(0, 1);
+      let x = (40 + offset) * Math.sin(a * v + d + j / 5) + bt.randInRange(0, 1);
+      let y = (40 + offset) * Math.sin(b * v + j / 10) + bt.randInRange(-1, 0);
 
-      if (i === 0) {
-        t.jump([62.5 + x, 62.5 + y]);
-        t.down();
-      } else {
-        let dx = (62.5 + x) - px;
-        let dy = (62.5 + y) - py;
-        let dist = Math.sqrt(dx * dx + dy * dy);
-        let ang = Math.atan2(dy, dx) * 180 / Math.PI;
+      let size = 2 + (i % 4) * -0.6;
 
-        t.setAngle(ang);
-        t.forward(dist);
-      }
+      drawSquare(t, 62.5 + x, 62.5 + y, size);
 
       px = 62.5 + x;
       py = 62.5 + y;
     }
-    t.up();
   }
 }
 
@@ -44,7 +44,7 @@ function drawBorder() {
   let border = new Turtle();
   border.jump([0, 0]);
   border.down();
-
+  
   border.forward(125);
   border.left(90);
   border.forward(125);
@@ -52,14 +52,14 @@ function drawBorder() {
   border.forward(125);
   border.left(90);
   border.forward(125);
-
+  
   return border.lines();
 }
 
 setDocDimensions(125, 125);
 
 let t = new Turtle();
-drawComplexLissajous(t, 8.1, 4, Math.PI / TILT, 500, 2);
+draw(t, 5, 3, Math.PI / 3, 97, 9);
 
 drawLines(t.lines());
 
