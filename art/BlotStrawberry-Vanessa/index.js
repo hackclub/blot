039094cf3397d3360a98cb851d2e8d4f9dece114
Vanessa Tan <140679670+vanessatan2007@users.noreply.github.com>
2024-08-30 21:59:32 -0400

    new seeds, random leaves

--- art/BlotStrawberry-Vanessa/index.js
+++ art/BlotStrawberry-Vanessa/index.js
@@ -9,24 +9,36 @@ const height = 125
 
 setDocDimensions(width, height)
 
-const sharpness = bt.randIntInRange(35,42)
-const tall = bt.randIntInRange(85,92)
-const berry = bt.catmullRom([[75, 25], [90, sharpness], [100, 80], [75, tall], [50,80], [60,sharpness], [75, 25]])
-const leaves = bt.catmullRom([[75, tall], [bt.randIntInRange(47,52), tall+bt.randIntInRange(3,8)], [bt.randIntInRange(47,52), tall+bt.randIntInRange(9, 16)], [75, tall], [bt.randIntInRange(53,65), tall+bt.randIntInRange(8,12)],[bt.randIntInRange(53,65), tall+bt.randIntInRange(12,17)], [75,tall], [bt.randIntInRange(66,72),tall + bt.randIntInRange(6,11)],[bt.randIntInRange(68,78), tall+bt.randIntInRange(10,19)], [75, tall], [bt.randIntInRange(76,82), tall+bt.randIntInRange(3,9)], [bt.randIntInRange(80,87), tall+bt.randIntInRange(11,15)], [75, tall], [bt.randIntInRange(85,92), tall+bt.randIntInRange(5,12)], [bt.randIntInRange(94,100), tall+bt.randIntInRange(11,19)], [75, tall]])
+const sharpness = bt.randIntInRange(35,42) //y coord of berry side
+const tall = bt.randIntInRange(85,92) //max y coord of berry
+const length = bt.randIntInRange(18,30) //min y coord of berry
+const berry = bt.catmullRom([[75, length], [90, sharpness], [100, 80], [75, tall], [50,80], [60,sharpness], [75, length]])
 drawLines([berry])
-drawLines([leaves])
 
-//seeds
+
+//leaves
+for (let i = 0;i<bt.randIntInRange(4,8);i++) { 
+  const startx = bt.randIntInRange(47,100);
+  const ofsetx = bt.randIntInRange(3,16); //offset of x from starting
+  const ofsety = bt.randIntInRange(3,16);
+  const starty = bt.randIntInRange(85,92);
+  const line =  bt.catmullRom([[75,tall],[startx, starty],[startx+ofsetx, starty+ofsety],[75,tall]])
+  drawLines([line])
+}
+
 const seeds = bt.randIntInRange(8, 12);
 for (let i = 0;i<seeds;i++) { 
   const startx = bt.randIntInRange(65,80);
   const starty = bt.randIntInRange(35, tall-10);
-  const seed =  bt.catmullRom([[startx, starty], [startx+ bt.randIntInRange(1,2),starty+bt.randIntInRange(1,3)]])
+  const seed =  bt.catmullRom([[startx, starty], [startx+0.5,starty-0.5],[startx- bt.randIntInRange(1,2),starty-bt.randIntInRange(1,3)],[startx-0.5, starty-0.5],[startx,starty]])
   drawLines([seed])
 }
+
+
 for (let i = 0;i<seeds;i++) { 
   const startx = bt.randIntInRange(65,80);
   const starty = bt.randIntInRange(35, tall-10);
-  const seed =  bt.catmullRom([[startx+ bt.randIntInRange(1,2),starty+bt.randIntInRange(1,3)],[startx, starty]])
+  const seed =  bt.catmullRom([[startx, starty], [startx+0.5,starty-0.5],[startx+ bt.randIntInRange(1,2),starty-bt.randIntInRange(1,3)],[startx-0.5, starty-0.5],[startx,starty]])
   drawLines([seed])
 }
+
