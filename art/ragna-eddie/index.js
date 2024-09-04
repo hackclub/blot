3429403c8e332565cdf22873588d3b8a6541cfb5
Eddie <121364681+B-Eddie@users.noreply.github.com>
2024-09-03 20:46:11 -0400

    Update index.js

--- art/ragna-eddie/index.js
+++ art/ragna-eddie/index.js
@@ -1,7 +1,8 @@
-// welcome to blot!
-
-// check out this guide to learn how to program in blot
-// https://blot.hackclub.com/editor?guide=start
+/*
+@title: ragna
+@author: eddie
+@snapshot: 0.png
+*/
 
 const width = 125;
 const height = 125;
