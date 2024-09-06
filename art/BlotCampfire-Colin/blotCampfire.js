e6b6df618cb4a6e21642d33ca5df62b54f40b661
Colin VanderMeer <colinvandermeer@gmail.com>
2024-09-06 15:46:18 -0400

    Update blotCampfire.js

--- art/BlotCampfire-Colin/blotCampfire.js
+++ art/BlotCampfire-Colin/blotCampfire.js
@@ -10,10 +10,13 @@ const height = 125;
 setDocDimensions(width, height);
 
 bt.setRandSeed = 128;
-const numberOfFlames = bt.randInRange(3, 6);
+
+const logNum = Math.floor(bt.randInRange(1, 4))
 
 const logTurtle = new bt.Turtle()
 const flameTurtle = new bt.Turtle()
+
+let logLines = []
 
 function drawCircle(startX, startY, radius) {
   logTurtle.jump([startX, startY - radius]);
@@ -21,22 +24,76 @@ function drawCircle(startX, startY, radius) {
 }
 
 //Create Logs
-logTurtle.down();
-drawCircle(30, 25, 10);
-drawCircle(95, 25, 10);
-drawCircle(30, 50, 10);
-drawCircle(95, 50, 10);
+switch(logNum) {
+  case 1:
+    drawCircle(30, 35, 10);
+    drawCircle(95, 35, 10);
 
-logTurtle.jump([30, 35]);
-logTurtle.goTo([95, 60]);
-logTurtle.jump([30, 15]);
-logTurtle.goTo([95, 40]);
+    const rect11 = [
+      [30, 45],
+      [95, 45],
+      [95, 25],
+      [30, 25],
+      [30, 45],
+    ]
+    logLines.push(rect11)
+    break;
+  case 2:
+    logTurtle.down();
+    drawCircle(30, 25, 10);
+    drawCircle(95, 25, 10);
+    drawCircle(30, 50, 10);
+    drawCircle(95, 50, 10);
+    
+    const rect21 = [
+      [30, 60],
+      [95, 35],
+      [95, 15],
+      [30, 40],
+      [30, 60],
+    ]
+    logLines.push(rect21)
+    
+    const rect22 = [
+      [30, 35],
+      [95, 60],
+      [95, 40],
+      [30, 15],
+      [30, 35],
+    ]
+    logLines.push(rect22)
 
-logTurtle.jump([30, 60]);
-logTurtle.goTo([95, 35]);
-logTurtle.jump([30, 40]);
-logTurtle.goTo([95, 15]);
-drawLines(logTurtle.lines(), { stroke: "brown", width: 3 });
+    break;
+  case 3:
+    drawCircle(30, 25, 10);
+    drawCircle(95, 25, 10);
+    drawCircle(30, 50, 10);
+    drawCircle(95, 50, 10);
+    drawCircle(63, 20, 10);
+    
+    const rect31 = [
+      [30, 60],
+      [95, 35],
+      [95, 15],
+      [30, 40],
+      [30, 60],
+    ]
+    logLines.push(rect31)
+    
+    const rect32 = [
+      [30, 35],
+      [95, 60],
+      [95, 40],
+      [30, 15],
+      [30, 35],
+    ]
+    logLines.push(rect32)
+    
+
+    break;
+}
+    drawLines(logTurtle.lines(), { stroke: "brown", width: 3, fill: "brown" });
+    drawLines(logLines, { stroke: "brown", width: 3, fill: "brown" });
 
 flameTurtle.jump([30, 70]);
 flameTurtle.right(90);
@@ -44,15 +101,22 @@ flameTurtle.arc(180, 32.5);
 
 flameTurtle.left(20)
 
-for (let i = 0; i <= numberOfFlames; i++) {
-  let flameAngle = bt.randInRange(120, 170);
-  flameTurtle.forward(80 / numberOfFlames);
-  flameTurtle.left(flameAngle);
-  flameTurtle.forward(80 / numberOfFlames);
-  flameTurtle.right(flameAngle);
+let flame = [];
+let flameX = 95;
+
+flame.push([95, 70]);
+
+while (true) {
+  flameX -= Math.floor(bt.randInRange(4, 8));
+  if (flameX < 30) { break } 
+  const flameY = Math.floor(bt.randInRange(120, 70));
+  flame.push([flameX, flameY]);
 }
 
-flameTurtle.goTo([30, 70])
+flame.push([30, 70]);
 
-drawLines(flameTurtle.lines(), { stroke: "red", width: 10 });
+
+drawLines(flameTurtle.lines(), { stroke: "red", width: 10, fill: "red" });
 drawLines(flameTurtle.lines(), { stroke: "orange", width: 3 });
+drawLines([bt.nurbs(flame)], { stroke: "red", width: 10, fill: "red" });
+drawLines([bt.nurbs(flame)], { stroke: "orange", width: 3 });
