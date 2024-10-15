d1c57e1645367f5286c0d5eebdfb546462927707
anomynous1225 <elijahbpierce@gmail.com>
2024-10-15 15:26:10 -0400

    Add files via upload

--- art/snowMelt-elijahPierce/project.js
+++ art/snowMelt-elijahPierce/project.js
@@ -114,7 +114,545 @@ if (1 == chance) {
   drawLines(bt.scale(hat.path, [1, 1]), { fill: "black" });
   drawLines(bt.scale(sun.path, [1, 1]));
   drawLines(bt.scale(penguin_body.path, [1, 1]));
-} else {
+} 
+else if (2 == chance) {
+  const boltX = bt.randIntInRange(15,90)
+  const boat = new bt.Turtle()
+    //boat
+    .jump([30,63])
+    .forward(60)
+    .arc(40,15)
+    .right(130)
+    .forward(14)
+    .left(130)
+    .right(62)
+    .jump([20,53])
+    .arc(45,105)
+    .right(60)
+    .jump([21,66])
+    .arc(40,15)
+    .right(93)
+    .jump([21,66])
+    .forward(14)
+    .left(68)
+    .jump([20,63])
+    .arc(45,105)
+    .left(246)
+    .jump([30,63])
+    .forward(3)
+    .jump([40,63])
+    .forward(6)
+    .jump([50,63])
+    .forward(7)
+    .jump([60,63])
+    .forward(8)
+    .jump([70,63])
+    .forward(7)
+    .jump([80,63])
+    .forward(6)
+    .jump([90,63])
+    .forward(3)
+  const water = new bt.Turtle()
+    //waves
+    .jump([0,50])
+    .arc(30,30)
+    .right(60)
+    .jump([15,54])
+    .arc(30,30)
+    .right(180)
+    .forward(30)
+    .left(180)
+    .jump([30,50])
+    .arc(30,30)
+    .right(60)
+    .jump([45,54])
+    .arc(30,30)
+    .right(180)
+    .forward(30)
+    .left(180)
+    .jump([60,50])
+    .arc(30,30)
+    .right(60)
+    .jump([75,54])
+    .arc(30,30)
+    .right(180)
+    .forward(30)
+    .left(180)
+    .jump([95,51])
+    .arc(30,30)
+    .right(45)
+    .forward(15)
+    .right(33)
+    .left(20)
+    .jump([110,55])
+    .arc(30,30)
+    //main water
+    .jump([0,50])
+    .right(92)
+    .forward(50)
+    .left(90)
+    .forward(125)
+    .left(90)
+    .forward(51)
+  const sun = new bt.Turtle()
+    .jump([100, 105])
+    .arc(360, 7)
+    //lines
+    .jump([107, 112])
+    .forward(7)
+    .jump([86, 112])
+    .forward(7)
+    .jump([105, 117])
+    .left(45)
+    .forward(7)
+    .jump([89, 104])
+    .forward(7)
+    .right(90)
+    .jump([106.5, 109])
+    .forward(7)
+    .right(45)
+    .jump([100, 105])
+    .forward(7)
+    .left(45)
+    .jump([89, 121])
+    .forward(7)
+    .right(45)
+    .jump([100, 124])
+    .forward(5);
+  const lightning = new bt.Turtle()
+    .right(90)
+    .jump([boltX,125])
+    .right(15)
+    .forward(20)
+    .left(105)
+    .forward(7)
+    .right(130)
+    .forward(30)
+    
+  drawLines(bt.scale(sun.path, [1, 1]));
+  drawLines(bt.scale(lightning.path, [1, 1]));
+  drawLines(bt.scale(boat.path, [1, 1]));
+  drawLines(bt.scale(water.path, [1, 1]),{fill:"black"});
+}
+else if (3 == chance) {
+  const iceburg = new bt.Turtle()
+    //main peak
+    .jump([7,50])
+    .left(40)
+    .forward(60)
+    .right(80)
+    .forward(30)
+    //secondary peak
+    .left(80)
+    .forward(10)
+    .right(80)
+    .forward(40)
+    .right(140)
+    .forward(107)
+  const water = new bt.Turtle()
+    //waves
+    .jump([0,50])
+    .arc(30,30)
+    .right(60)
+    .jump([15,54])
+    .arc(30,30)
+    .right(180)
+    .forward(30)
+    .left(180)
+    .jump([30,50])
+    .arc(30,30)
+    .right(60)
+    .jump([45,54])
+    .arc(30,30)
+    .right(180)
+    .forward(30)
+    .left(180)
+    .jump([60,50])
+    .arc(30,30)
+    .right(60)
+    .jump([75,54])
+    .arc(30,30)
+    .right(180)
+    .forward(30)
+    .left(180)
+    .jump([95,51])
+    .arc(30,30)
+    .right(45)
+    .forward(15)
+    .right(33)
+    .left(20)
+    .jump([110,55])
+    .arc(30,30)
+    //main water
+    .jump([0,50])
+    .right(92)
+    .forward(50)
+    .left(90)
+    .forward(125)
+    .left(90)
+    .forward(51)
+  const snow = new bt.Turtle()
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5);
+  
+  drawLines(bt.scale(water.path, [1, 1]),{fill:"black"});
+  drawLines(bt.scale(iceburg.path, [1, 1]));
+  drawLines(bt.scale(snow.path, [1, 1]));
+}
+else if (4 == chance){
+  const boat = new bt.Turtle()
+    //boat
+    .jump([50,63])
+    .forward(60)
+    .arc(40,15)
+    .right(130)
+    .forward(14)
+    .left(130)
+    .right(62)
+    .jump([40,53])
+    .arc(45,105)
+    .right(60)
+    .jump([41,66])
+    .arc(40,15)
+    .right(93)
+    .jump([41,66])
+    .forward(14)
+    .left(68)
+    .jump([40,63])
+    .arc(45,105)
+    .left(246)
+    .jump([110,63])
+    .forward(3)
+    .jump([100,63])
+    .forward(6)
+    .jump([70,63])
+    .forward(7)
+    .jump([80,63])
+    .forward(8)
+    .jump([90,63])
+    .forward(7)
+    .jump([60,63])
+    .forward(6)
+    .jump([50,63])
+    .forward(3)
+  const iceburg = new bt.Turtle()
+    //main peak
+    .jump([2,50])
+    .left(70)
+    .forward(40)
+    .right(120)
+    .forward(20)
+    //secondary peak
+    .left(100)
+    .forward(5)
+    .right(100)
+    .forward(35)
+    .right(130)
+    .forward(39)
+  const water = new bt.Turtle()
+    //waves
+    .jump([0,50])
+    .arc(30,30)
+    .right(60)
+    .jump([15,54])
+    .arc(30,30)
+    .right(180)
+    .forward(30)
+    .left(180)
+    .jump([30,50])
+    .arc(30,30)
+    .right(60)
+    .jump([45,54])
+    .arc(30,30)
+    .right(180)
+    .forward(30)
+    .left(180)
+    .jump([60,50])
+    .arc(30,30)
+    .right(60)
+    .jump([75,54])
+    .arc(30,30)
+    .right(180)
+    .forward(30)
+    .left(180)
+    .jump([95,51])
+    .arc(30,30)
+    .right(45)
+    .forward(15)
+    .right(33)
+    .left(20)
+    .jump([110,55])
+    .arc(30,30)
+    //main water
+    .jump([0,50])
+    .right(92)
+    .forward(50)
+    .left(90)
+    .forward(125)
+    .left(90)
+    .forward(51)
+  const snow = new bt.Turtle()
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5)
+    .jump([
+    bt.randIntInRange(1, 125),
+    bt.randIntInRange(1, 125)
+  ])
+    .arc(360, .5);
+
+  drawLines(bt.scale(boat.path, [1,1]));
+  drawLines(bt.scale(water.path, [1, 1]),{fill:"black"});
+  drawLines(bt.scale(iceburg.path, [1, 1]));
+  drawLines(bt.scale(snow.path, [1, 1]));
+}
+else if(5 < chance){
+  const astroidX = bt.randIntInRange(10,115)
+  const volcano = new bt.Turtle()
+    //base
+    .jump([38,50])
+    .right(284)
+    .forward(50)
+    .right(77)
+    .forward(7) 
+    .right(90)
+    .forward(50)
+    .left(90)
+    .forward(6)
+    .left(90)
+    .forward(50)
+    .right(90)
+    .forward(7)
+    .left(77)
+    .right(151)
+    .forward(50)
+    .right(105)
+    .forward(45)
+    .right(180)
+  //ground
+    .jump([0,50])
+    .forward(125)
+  const smoke = new bt.Turtle()
+    .jump([59,93])
+    .left(60)
+    .forward(13)
+    .right(60)
+    .arc(135,7)
+    .right(60)
+    .arc(135,7)
+    .right(60)
+    .arc(135,7)
+    .right(60)
+    .arc(135,7)
+    .right(60)
+    .forward(13)
+    .left(60)
+  const astroid = new bt.Turtle()
+    //circle
+    .jump([astroidX,110])
+    .arc(360,4)
+    //tail
+    .jump([astroidX-14,116])
+    .forward(10)
+    .right(70)
+    .forward(4)
+    
+
+  drawLines(bt.scale(astroid.path,[1,1]),{fill:"black"});
+  drawLines(bt.scale(smoke.path, [1,1]));
+  drawLines(bt.scale(volcano.path, [1,1]),{fill:"black"});
+}
+else {
   const igloo = new bt.Turtle()
     //big circle
     .jump([31, 45])
@@ -332,7 +870,6 @@ if (1 == chance) {
     .jump([0, 45])
     .right(174)
     .forward(125);
-
 
 
 
