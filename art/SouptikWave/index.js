7f23afd8b8bd3c523ee88c923d1de7efd011fef0
Souptik Samanta <161435060+souptik-samanta@users.noreply.github.com>
2024-11-23 18:48:36 +0530

    done

--- art/SouptikWave/index.js
+++ art/SouptikWave/index.js
@@ -3,63 +3,64 @@
 @author: souptik samanta
 @snapshot: snapshot1.png
 */
-const GRID_UNIT = 6;
-const FORCE_MAG = 3;
-const DOC_W = 150;
-const DOC_H = 150;
-const MAX_ITERATIONS = 100;
-const STEP_SIZE = 1;
-const CURVE_SPACING = 4;
 
-let vectorField = [];
-const seedValue = Date.now();
+const gu = 6;
+const fm = 3;
+const dw = 150;
+const dh = 150;
+const mi = 100;
+const ss = 1;
+const cs = 4;
 
-function mapValue(value, inStart, inEnd, outStart, outEnd, clamp = false) {
-    let mapped = outStart + ((value - inStart) * (outEnd - outStart)) / (inEnd - inStart);
-    if (clamp) mapped = Math.max(outStart, Math.min(mapped, outEnd));
+let vf = [];
+const sv = Date.now();
+
+function mv(val, is, ie, os, oe, clamp = false) {
+    let mapped = os + ((val - is) * (oe - os)) / (ie - is);
+    if (clamp) mapped = Math.max(os, Math.min(mapped, oe));
     return mapped;
 }
 
-function computeAngle(x, y) {
-    const offset = Math.sin(seedValue * 0.0001 + x * 0.1) + Math.cos(seedValue * 0.0001 + y * 0.1);
-    return mapValue(offset * 0.5, -1, 1, 0, Math.PI * 2);
+function ca(x, y) {
+    const off = Math.sin(sv * 0.0001 + x * 0.1) + Math.cos(sv * 0.0001 + y * 0.1);
+    return mv(off * 0.5, -1, 1, 0, Math.PI * 2);
 }
 
-function initializeGrid(width, height, angleFn) {
-    setDocDimensions(width, height);
-    for (let i = 0; i < width / GRID_UNIT; i++) {
-        vectorField[i] = [];
-        for (let j = 0; j < height / GRID_UNIT; j++) {
-            const angle = angleFn(i * GRID_UNIT, j * GRID_UNIT);
-            vectorField[i][j] = new Vector(FORCE_MAG, angle, false);
+function ig(w, h, angleFn) {
+    setDocDimensions(w, h);
+    for (let i = 0; i < w / gu; i++) {
+        vf[i] = [];
+        for (let j = 0; j < h / gu; j++) {
+            const angle = angleFn(i * gu, j * gu);
+            vf[i][j] = new Vector(fm, angle, false);
         }
     }
 }
 
-function generateCurves() {
-    for (let x = 0; x < DOC_W; x += CURVE_SPACING) {
-        for (let y = 0; y < DOC_H; y += CURVE_SPACING) {
-            renderCurve(MAX_ITERATIONS, STEP_SIZE, new Coord(x, y));
+function gc() {
+    for (let x = 0; x < dw; x += cs) {
+        for (let y = 0; y < dh; y += cs) {
+            rc(mi, ss, new Coord(x, y));
         }
     }
 }
 
-function renderCurve(iterations, stepDist, start) {
+function rc(it, sd, start) {
     let pathPoints = [];
-    let position = start;
+    let pos = start;
 
-    for (let i = 0; i < iterations; i++) {
-        const col = Math.floor(position.x / GRID_UNIT);
-        const row = Math.floor(position.y / GRID_UNIT);
+    for (let i = 0; i < it; i++) {
+        const col = Math.floor(pos.x / gu);
+        const row = Math.floor(pos.y / gu);
 
-        if (row < 0 || row >= vectorField.length || col < 0 || col >= vectorField[0].length) break;
+        if (row < 0 || row >= vf.length || col < 0 || col >= vf[0].length) break;
 
-        pathPoints.push(position.toArray());
-        const angle = vectorField[col][row].angle;
-        position.x += stepDist * Math.cos(angle);
-        position.y += stepDist * Math.sin(angle);
+        pathPoints.push(pos.toArray());
+        const angle = vf[col][row].angle;
+        pos.x += sd * Math.cos(angle);
+        pos.y += sd * Math.sin(angle);
 
-        if (position.x < 0 || position.x >= DOC_W || position.y < 0 || position.y >= DOC_H) break;
+        if (pos.x < 0 || pos.x >= dw || pos.y < 0 || pos.y >= dh) break;
     }
 
     if (pathPoints.length > 2) {
@@ -75,9 +76,9 @@ class Coord {
     }
 
     angleTo(other) {
-        const deltaY = other.y - this.y;
-        const deltaX = other.x - this.x;
-        return Math.atan2(deltaY, deltaX);
+        const dy = other.y - this.y;
+        const dx = other.x - this.x;
+        return Math.atan2(dy, dx);
     }
 
     toArray() {
@@ -86,12 +87,12 @@ class Coord {
 }
 
 class Vector {
-    constructor(magnitude, angle, degrees = true) {
-        if (degrees) angle *= Math.PI / 180;
-        this.x = magnitude * Math.cos(angle);
-        this.y = magnitude * Math.sin(angle);
-        this.magnitude = magnitude;
-        this.angle = angle;
+    constructor(m, a, deg = true) {
+        if (deg) a *= Math.PI / 180;
+        this.x = m * Math.cos(a);
+        this.y = m * Math.sin(a);
+        this.m = m;
+        this.angle = a;
     }
 
     drawVector(x = 0, y = 0) {
@@ -104,22 +105,22 @@ class Vector {
 }
 
 class LineStyle {
-    constructor(thickness, color) {
-        this.thickness = thickness;
-        this.color = color;
+    constructor(t, c) {
+        this.t = t;
+        this.c = c;
     }
 
     applyToPath(path) {
-        drawLines([path], { width: this.thickness, stroke: this.color });
+        drawLines([path], { width: this.t, stroke: this.c });
     }
 }
 
-function buildLayeredPaths(numLayers, layerSpacing, customThickness) {
-    for (let layer = 0; layer < numLayers; layer++) {
-        const offset = layer * layerSpacing;
-        for (let x = offset; x < DOC_W; x += CURVE_SPACING) {
-            for (let y = offset; y < DOC_H; y += CURVE_SPACING) {
-                const customPath = traceLayeredPath(MAX_ITERATIONS, STEP_SIZE, new Coord(x, y), layer, customThickness);
+function blp(nl, ls, ct) {
+    for (let layer = 0; layer < nl; layer++) {
+        const offset = layer * ls;
+        for (let x = offset; x < dw; x += cs) {
+            for (let y = offset; y < dh; y += cs) {
+                const customPath = tlp(mi, ss, new Coord(x, y), layer, ct);
                 const style = new LineStyle(layer + 1, "#000000");
                 style.applyToPath(customPath);
             }
@@ -127,42 +128,42 @@ function buildLayeredPaths(numLayers, layerSpacing, customThickness) {
     }
 }
 
-function traceLayeredPath(maxSteps, moveLength, startPos, layerIndex, thickness) {
+function tlp(ms, ml, startPos, layerIndex, thickness) {
     let pointPath = [];
-    let currentPos = startPos;
+    let curPos = startPos;
 
-    for (let step = 0; step < maxSteps; step++) {
-        const col = Math.floor(currentPos.x / GRID_UNIT);
-        const row = Math.floor(currentPos.y / GRID_UNIT);
+    for (let step = 0; step < ms; step++) {
+        const col = Math.floor(curPos.x / gu);
+        const row = Math.floor(curPos.y / gu);
 
-        if (row < 0 || row >= vectorField.length || col < 0 || col >= vectorField[0].length) break;
+        if (row < 0 || row >= vf.length || col < 0 || col >= vf[0].length) break;
 
-        pointPath.push(currentPos.toArray());
-        const directionalAngle = vectorField[col][row].angle + layerIndex * 0.01;
-        currentPos.x += moveLength * Math.cos(directionalAngle);
-        currentPos.y += moveLength * Math.sin(directionalAngle);
+        pointPath.push(curPos.toArray());
+        const dirAngle = vf[col][row].angle + layerIndex * 0.01;
+        curPos.x += ml * Math.cos(dirAngle);
+        curPos.y += ml * Math.sin(dirAngle);
 
-        if (currentPos.x < 0 || currentPos.x >= DOC_W || currentPos.y < 0 || currentPos.y >= DOC_H) break;
+        if (curPos.x < 0 || curPos.x >= dw || curPos.y < 0 || curPos.y >= dh) break;
     }
 
     return pointPath;
 }
 
-function randomizeGridValues(seedBase) {
-    for (let i = 0; i < vectorField.length; i++) {
-        for (let j = 0; j < vectorField[i].length; j++) {
-            const modAngle = computeAngle(seedBase + i, seedBase + j);
-            vectorField[i][j].angle = modAngle;
+function rv(seedBase) {
+    for (let i = 0; i < vf.length; i++) {
+        for (let j = 0; j < vf[i].length; j++) {
+            const modAngle = ca(seedBase + i, seedBase + j);
+            vf[i][j].angle = modAngle;
         }
     }
 }
 
-function generatePatterns(seedModifier) {
-    randomizeGridValues(seedModifier);
-    initializeGrid(DOC_W, DOC_H, computeAngle);
-    buildLayeredPaths(3, 2, 1.5);
+function gp(sm) {
+    rv(sm);
+    ig(dw, dh, ca);
+    blp(3, 2, 1.5);
 }
 
-initializeGrid(DOC_W, DOC_H, computeAngle);
-generateCurves();
-generatePatterns(50);
+ig(dw, dh, ca);
+gc();
+gp(50);
